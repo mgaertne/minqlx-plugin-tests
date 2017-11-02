61d4dc04b1e246aa12bcd7d3d961fdc64bc5dbd8
Markus Gärtner <markus.gaertner@it-agile.de>
2017-11-02 01:24:29 +0000

    fiddled around a bit with the pyb build script

--- build.py
+++ build.py
@@ -1,6 +1,10 @@
-from pybuilder.core import init, use_plugin
+#!/usr/bin/env python
+#
 
-use_plugin("python.core")
+#   -*- coding: utf-8 -*-
+
+from pybuilder.core import init, use_plugin, Author
+
 use_plugin("python.install_dependencies")
 
 use_plugin("python.unittest")
@@ -8,21 +12,29 @@ use_plugin("python.coverage")
 
 use_plugin("python.flake8")
 
+use_plugin("python.sphinx")
+
 use_plugin("python.pycharm")
 use_plugin("python.pydev")
 
-default_task = "publish"
+default_task = ["install_dependencies", "analyze", "run_unit_tests"]
+
+version = "0.0.1"
+authors = (Author("Markus 'ShiN0' Gaertner"),)
+url = "https://github.com/mgaertne/minqlx-plugin-tests"
+
+requires_python = ">=2.7,!=3.0,!=3.1,!=3.2,!=3.3,<3.8"
+
 
 @init
 def initialize(project):
-    project.build_depends_on('mockito')
-    project.build_depends_on('PyHamcrest')
+    project.build_depends_on_requirements('requirements.txt')
 
-    project.set_property("coverage_threshold_warn", 0)
+    project.set_property("coverage_break_build", False)
     project.set_property("coverage_exceptions",
-                         ["minqlx.__init__", "minqlx._commands", "minqlx._core", "minqlx._events", "minqlx._game",
-                          "minqlx._handlers", "minqlx._minqlx", "minqlx._player", "minqlx._plugin", "minqlx._zmq",
-                          "minqlx.database"])
+                         ["__init__/__init__.py", "minqlx.__init__.py", "minqlx._commands", "minqlx._core",
+                          "minqlx._events", "minqlx._game", "minqlx._handlers", "minqlx._minqlx", "minqlx._player",
+                          "minqlx._plugin", "minqlx._zmq", "minqlx.database"])
 
     project.set_property("flake8_include_test_sources", True)
     project.set_property("flake8_ignore", "F403,F405")
