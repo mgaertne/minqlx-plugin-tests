de3b7aa76755ae9d61546f3533c64bfadb7a6e4c
Markus Gärtner <markus.gaertner@it-agile.de>
2017-11-02 21:08:29 +0100

    moved functions closer to their first usage positions in the class

--- src/main/python/duelarena.py
+++ src/main/python/duelarena.py
@@ -14,7 +14,6 @@ class duelarena(minqlx.Plugin):
     """
 
     def __init__(self):
-
         self.add_hook("team_switch_attempt", self.handle_team_switch_event)
         self.add_hook("player_disconnect", self.handle_player_disco)
         self.add_hook("player_connect", self.handle_player_connect)
@@ -71,13 +70,49 @@ class duelarena(minqlx.Plugin):
             self.center_print(DUEL_ARENA_ANNOUNCEMENT)
             self.msg(DUEL_ARENA_ANNOUNCEMENT)
 
-    def handle_round_countdown(self, *args, **kwargs):
+    def switch_duelarena_if_necessary(self):
+        self.checklists()
 
+        if self.duelmode and not self.should_duelmode_be_activated():
+            self.deactivate_duelarena_mode()
+            return
+
+        if not self.duelmode and self.should_duelmode_be_activated():
+            self.activate_duelarena_mode()
+
+    def checklists(self):
+        self.queue[:] = [sid for sid in self.queue if self.player(sid) and self.player(sid).ping < 990]
+        self.psub[:] = [sid for sid in self.psub if self.player(sid) and self.player(sid).ping < 990]
+
+    def should_duelmode_be_activated(self):
+        player_count = self.count_connected_players()
+
+        return player_count in range(MIN_ACTIVE_PLAYERS, MAX_ACTIVE_PLAYERS + 1) \
+            and len(self.psub) >= MIN_ACTIVE_PLAYERS
+
+    def deactivate_duelarena_mode(self):
+        self.duelmode = False
+        self.msg("DuelArena has been deactivated! You are free to join.")
+
+    def activate_duelarena_mode(self):
+        self.duelmode = True
+        self.msg("DuelArena activated!")
+        self.center_print("DuelArena activated!")
+        if self.game and self.game.state == "in_progress":
+            self.initduel = True
+
+    def count_connected_players(self):
+        return len(self.players())
+
+    def handle_round_countdown(self, *args, **kwargs):
         if not self.duelmode:
             return
 
+        self.center_print(self.round_announcement())
+
+    def round_announcement(self):
         teams = self.teams()
-        self.center_print("{} ^2vs {}".format(teams["red"][-1].name, teams["blue"][-1].name))
+        return "{} ^2vs {}".format(teams["red"][-1].name, teams["blue"][-1].name)
 
     # When a game is about to start and duelmode is active, initialize
     @minqlx.delay(3)
@@ -88,6 +123,60 @@ class duelarena(minqlx.Plugin):
         if not self.duelmode:
             return
         self.init_duel()
+
+    def init_duel(self):
+        self.checklists()
+
+        self.insert_subscribed_players_to_queue_if_necessary()
+
+        player1 = self.player(self.queue.pop())
+        player2 = self.player(self.queue.pop())
+        self.move_players_to_teams(player1, player2)
+
+        self.move_all_non_playing_players_to_spec(player1, player2)
+
+        self.initduel = False
+
+    def insert_subscribed_players_to_queue_if_necessary(self):
+        for player in [steam_id for steam_id in self.psub if steam_id not in self.queue]:
+            self.append_player_to_end_of_queue(player)
+
+    def append_player_to_end_of_queue(self, player):
+        self.queue.insert(0, player)
+
+    def move_players_to_teams(self, player1, player2):
+        teams = self.teams()
+
+        if player1 in teams["red"]:
+            if player2 not in teams["blue"]:
+                self.put_player_on_team(player2, "blue")
+            return
+
+        if player1 in teams["blue"]:
+            if player2 not in teams["red"]:
+                self.put_player_on_team(player2, "red")
+            return
+
+        if player2 in teams["blue"]:
+            self.put_player_on_team(player1, "red")
+            return
+
+        if player2 in teams["red"]:
+            self.put_player_on_team(player1, "blue")
+            return
+
+        self.put_player_on_team(player1, "red")
+        self.put_player_on_team(player2, "blue")
+
+    def put_player_on_team(self, player, team):
+        self.switching_players.append(player)
+        player.put(team)
+
+    def move_all_non_playing_players_to_spec(self, *players):
+        teams = self.teams()
+
+        for player in [player for player in teams['red'] + teams['blue'] if player not in players]:
+            player.put("spectator")
 
     def handle_game_end(self, data):
         if not self.game:
@@ -100,7 +189,7 @@ class duelarena(minqlx.Plugin):
 
         teams = self.teams()
 
-        self.queue.insert(0, teams[loser][-1].steam_id)
+        self.append_player_to_end_of_queue(teams[loser][-1].steam_id)
         self.queue.append(teams[winner][-1].steam_id)
 
     def extract_winning_and_losing_team_from_game_end_data(self, data):
@@ -113,7 +202,6 @@ class duelarena(minqlx.Plugin):
         self.undelayed_handle_round_end(data)
 
     def undelayed_handle_round_end(self, data):
-
         # Not in CA? Do nothing
         if (not self.game) or (self.game.type_short != "ca"):
             return
@@ -130,21 +218,16 @@ class duelarena(minqlx.Plugin):
             return
 
         losing_team = self.extract_losing_team_from_round_end_data(data)
-        if not losing_team:
+        if losing_team is None:
             return  # Draw? Do nothing
 
-        next_player = self.player(self.queue.pop())
+        next_player = self.next_player()
 
-        teams = self.teams()
+        if next_player is None:
+            self.deactivate_duelarena_mode()
+            return
 
-        while not next_player or next_player not in teams['spectator']:
-            try:
-                next_player = self.player(self.queue.pop())
-            except IndexError:
-                self.deactivate_duelarena_mode()
-                return
-
-        losing_player = teams[losing_team][-1]
+        losing_player = self.weakest_player_on(losing_team)
 
         self.put_player_on_team(next_player, losing_team)
         self.put_player_to_spectators_and_back_in_duel_queue(losing_player)
@@ -156,16 +239,27 @@ class duelarena(minqlx.Plugin):
             return "red"
         return None
 
-    def put_player_on_team(self, player, team):
-        self.switching_players.append(player)
-        player.put(team)
+    def next_player(self):
+        next_player = self.player(self.queue.pop())
+
+        teams = self.teams()
+
+        while not next_player or next_player not in teams['spectator']:
+            try:
+                next_player = self.player(self.queue.pop())
+            except IndexError:
+                return None
+        return next_player
+
+    def weakest_player_on(self, losing_team):
+        teams = self.teams()
+        return teams[losing_team][-1]
 
     def put_player_to_spectators_and_back_in_duel_queue(self, losing_player):
-        self.queue.insert(0, losing_player.steam_id)
+        self.append_player_to_end_of_queue(losing_player.steam_id)
         losing_player.put("spectator")
 
     def cmd_duel(self, player, msg, channel):
-
         if self.count_connected_players() > MAX_ACTIVE_PLAYERS:
             player.tell(
                 "^6!duel^7 command not available with ^6{}^7 or more players connected".format(MAX_ACTIVE_PLAYERS + 1))
@@ -195,17 +289,18 @@ class duelarena(minqlx.Plugin):
         self.printqueue()
         self.activate_duelarena_mode()
 
-    def cmd_printqueue(self, player, msg, channel):
+    def player_is_subscribed(self, player):
+        return player.steam_id in self.psub
 
-        if self.count_connected_players() > MAX_ACTIVE_PLAYERS:
-            player.tell(
-                "^6!queue^7 command not available with ^6{}^7 or more players connected".format(MAX_ACTIVE_PLAYERS + 1))
-            return
+    def unsubscribe_player(self, player):
+        if self.player_is_enqueued(player):
+            self.queue.remove(player.steam_id)
+        self.psub.remove(player.steam_id)
 
-        self.printqueue()
+    def player_is_enqueued(self, player):
+        return player.steam_id in self.queue
 
     def printqueue(self):
-
         if len(self.queue) == 0:
             self.msg("There's no one in the queue yet. Type ^6!d ^7or ^6!duel ^7to enter the queue.")
             return
@@ -229,107 +324,15 @@ class duelarena(minqlx.Plugin):
     def position_of_player_in_queue(self, player):
         return len(self.queue) - self.queue.index(player.steam_id)
 
-    def init_duel(self):
-
-        self.checklists()
-
-        self.insert_subscribed_players_to_queue_if_necessary()
-
-        player1 = self.player(self.queue.pop())
-        player2 = self.player(self.queue.pop())
-        self.move_players_to_teams(player1, player2)
-
-        self.move_all_non_playing_players_to_spec(player1, player2)
-
-        self.initduel = False
-
-    def insert_subscribed_players_to_queue_if_necessary(self):
-        for _p in [steam_id for steam_id in self.psub if steam_id not in self.queue]:
-            self.queue.insert(0, _p)
-
-    def switch_duelarena_if_necessary(self):
-
-        self.checklists()
-
-        if self.duelmode and not self.should_duelmode_be_activated():
-            self.deactivate_duelarena_mode()
-            return
-
-        if not self.duelmode and self.should_duelmode_be_activated():
-            self.activate_duelarena_mode()
-
-    def checklists(self):
-
-        self.queue[:] = [sid for sid in self.queue if self.player(sid) and self.player(sid).ping < 990]
-
-        self.psub[:] = [sid for sid in self.psub if self.player(sid) and self.player(sid).ping < 990]
-
-    # Helper functions
-
-    def count_connected_players(self):
-        return len(self.players())
-
-    def player_is_subscribed(self, player):
-        return player.steam_id in self.psub
-
-    def player_is_enqueued(self, player):
-        return player.steam_id in self.queue
-
     def subscribe_player(self, player):
         if not self.player_is_enqueued(player):
-            self.queue.insert(0, player.steam_id)
+            self.append_player_to_end_of_queue(player.steam_id)
         self.psub.append(player.steam_id)
 
-    def unsubscribe_player(self, player):
-        if self.player_is_enqueued(player):
-            self.queue.remove(player.steam_id)
-        self.psub.remove(player.steam_id)
-
-    def should_duelmode_be_activated(self):
-        player_count = self.count_connected_players()
-
-        return player_count in range(MIN_ACTIVE_PLAYERS, MAX_ACTIVE_PLAYERS + 1) \
-            and len(self.psub) >= MIN_ACTIVE_PLAYERS
-
-    def activate_duelarena_mode(self):
-        self.duelmode = True
-        self.msg("DuelArena activated!")
-        self.center_print("DuelArena activated!")
-        if self.game and self.game.state == "in_progress":
-            self.initduel = True
-
-    def deactivate_duelarena_mode(self):
-        self.duelmode = False
-        self.msg("DuelArena has been deactivated! You are free to join.")
-
-    def move_players_to_teams(self, player1, player2):
-
-        teams = self.teams()
-
-        if player1 in teams["red"]:
-            if player2 not in teams["blue"]:
-                self.put_player_on_team(player2, "blue")
+    def cmd_printqueue(self, player, msg, channel):
+        if self.count_connected_players() > MAX_ACTIVE_PLAYERS:
+            player.tell(
+                "^6!queue^7 command not available with ^6{}^7 or more players connected".format(MAX_ACTIVE_PLAYERS + 1))
             return
 
-        if player1 in teams["blue"]:
-            if player2 not in teams["red"]:
-                self.put_player_on_team(player2, "red")
-            return
-
-        if player2 in teams["blue"]:
-            self.put_player_on_team(player1, "red")
-            return
-
-        if player2 in teams["red"]:
-            self.put_player_on_team(player1, "blue")
-            return
-
-        self.put_player_on_team(player1, "red")
-        self.put_player_on_team(player2, "blue")
-
-    def move_all_non_playing_players_to_spec(self, *players):
-
-        teams = self.teams()
-
-        for player in [player for player in teams['red'] + teams['blue'] if player not in players]:
-            player.put("spectator")
+        self.printqueue()
