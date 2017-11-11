594630ed015b3c5c7ac82e1e6a9d674396ab2833
Markus Gärtner <markus.gaertner@it-agile.de>
2017-11-11 14:35:51 +0100

    added tests for handling of game_end

--- src/unittest/python/duelarena2_tests.py
+++ src/unittest/python/duelarena2_tests.py
@@ -204,7 +204,7 @@ class DuelArenaTests(unittest.TestCase):
 
     def queue_up_players(self, *players):
         for player in players:
-            self.plugin.queue.append(player)
+            self.plugin.queue.append(player.steam_id)
 
     def test_handle_round_countdown_with_one_team_empty(self):
         red_player = fake_player(1, "Red Player", "red")
@@ -370,3 +370,83 @@ class DuelArenaTests(unittest.TestCase):
 
         self.assert_duelarena_deactivated()
 
+    def test_handle_game_end_with_no_game_ending(self):
+        setup_no_game()
+
+        return_code = self.plugin.handle_game_end({})
+
+        assert_that(return_code, is_(None))
+
+    def test_handle_game_end_with_not_duelarena_active(self):
+        self.deactivate_duelarena()
+
+        return_code = self.plugin.handle_game_end({})
+
+        assert_that(return_code, is_(None))
+
+    def test_handle_game_end_red_team_wins_puts_winner_back_to_queue(self):
+        red_player = fake_player(1, "Red Player", "red")
+        blue_player = fake_player(2, "Blue Player", "blue")
+        spec_player = fake_player(3, "Speccing Player")
+        connected_players(red_player, blue_player, spec_player)
+        self.setup_duelarena_players(red_player, blue_player, spec_player)
+        self.queue_up_players(spec_player)
+
+        self.plugin.handle_game_end({"TSCORE0": 8, "TSCORE1": 3})
+
+        self.assert_queue_contains(red_player, spec_player, blue_player)
+
+    def assert_queue_contains(self, *players):
+        player_ids = [player.steam_id for player in players]
+        player_ids.reverse()
+        assert_that(self.plugin.queue, is_(player_ids))
+
+    def test_handle_game_end_blue_team_wins_puts_winner_back_to_queue(self):
+        red_player = fake_player(1, "Red Player", "red")
+        blue_player = fake_player(2, "Blue Player", "blue")
+        spec_player = fake_player(3, "Speccing Player")
+        connected_players(red_player, blue_player, spec_player)
+        self.setup_duelarena_players(red_player, blue_player, spec_player)
+        self.queue_up_players(spec_player)
+
+        self.plugin.handle_game_end({"TSCORE0": 2, "TSCORE1": 8})
+
+        self.assert_queue_contains(blue_player, spec_player, red_player)
+
+    def test_handle_game_end_prints_results(self):
+        red_player = fake_player(1, "Red Player", "red")
+        blue_player = fake_player(2, "Blue Player", "blue")
+        spec_player = fake_player(3, "Speccing Player")
+        connected_players(red_player, blue_player, spec_player)
+        self.setup_duelarena_players(red_player, blue_player, spec_player)
+        self.queue_up_players(spec_player)
+        self.plugin.scores = {red_player.steam_id: 7, blue_player.steam_id: 5, spec_player.steam_id: 7}
+
+        self.plugin.handle_game_end({"TSCORE0": 8, "TSCORE1": 3})
+
+        assert_plugin_sent_to_console("DuelArena results:")
+        assert_plugin_sent_to_console("Place ^31.^7 Red Player ^7(Wins:^28^7)")
+        assert_plugin_sent_to_console("Place ^32.^7 Speccing Player ^7(Wins:^27^7)")
+        assert_plugin_sent_to_console("Place ^33.^7 Blue Player ^7(Wins:^25^7)")
+
+    def test_handle_game_end_winner_already_quit(self):
+        blue_player = fake_player(2, "Blue Player", "blue")
+        spec_player = fake_player(3, "Speccing Player")
+        connected_players(blue_player, spec_player)
+        self.setup_duelarena_players(fake_player(1, "Red Player", "red"), blue_player, spec_player)
+        self.queue_up_players(spec_player)
+
+        return_code = self.plugin.handle_game_end({"TSCORE0": 8, "TSCORE1": 3})
+
+        assert_that(return_code, is_(None))
+
+    def test_handle_game_end_loser_already_quit(self):
+        blue_player = fake_player(2, "Blue Player", "blue")
+        spec_player = fake_player(3, "Speccing Player")
+        connected_players(blue_player, spec_player)
+        self.setup_duelarena_players(fake_player(1, "Red Player", "red"), blue_player, spec_player)
+        self.queue_up_players(spec_player)
+
+        return_code = self.plugin.handle_game_end({"TSCORE0": 6, "TSCORE1": 8})
+
+        assert_that(return_code, is_(None))
