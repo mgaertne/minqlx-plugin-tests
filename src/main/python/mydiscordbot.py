b51a884d8201dfeaa4fcf1b47fd2849bf474524e
Markus Gärtner <markus.gaertner@it-agile.de>
2017-12-04 19:12:01 +0100

    another try to play with git attributes versioning

--- src/main/python/mydiscordbot.py
+++ src/main/python/mydiscordbot.py
@@ -30,7 +30,7 @@ from discord.ext import commands
 # regular expression, too!
 re_topic = re.compile(r".*players\. (.*)$")
 
-plugin_version = "v0.9.2"
+plugin_version = "v0.9.2-$Format:%h$"
 
 
 class mydiscordbot(minqlx.Plugin):
