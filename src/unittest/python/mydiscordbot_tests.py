8570ff2cf5c4a4eedd048b293d5b10a41fee8a63
Markus Gärtner <markus.gaertner@it-agile.de>
2018-01-02 18:48:49 +0100

    undid the changes from v1.0.0-pi to v1.0.0-rho to make it work on the busstation again

--- src/unittest/python/mydiscordbot_tests.py
+++ src/unittest/python/mydiscordbot_tests.py
@@ -30,7 +30,6 @@ class MyDiscordBotTests(unittest.TestCase):
         unstub()
 
     def test_constructor(self):
-        verify(self.discord).run()
         assert_plugin_sent_to_console(matches("mydiscordbot Version: "), atleast=1)
 
     def test_handle_unload_for_plugin(self):
