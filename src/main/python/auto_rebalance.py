d50d1995c995116353f69c1a31b648ffb95f6157
Markus Gärtner <markus.gaertner@it-agile.de>
2018-01-05 19:26:32 +0100

    now rebalancing also during team switch possible, and configurable, with unittests

--- src/main/python/auto_rebalance.py
+++ src/main/python/auto_rebalance.py
@@ -24,20 +24,115 @@ class auto_rebalance(minqlx.Plugin):
 
     Rebalances new players joined since the last round countdown based upon the ratings in the balance plugin to yield
     better balanced teams overall.
+
+    Uses:
+    * qlx_rebalanceMethod (default: "countdown") Describes the method for rebalancing. countdown will rebalance during
+    round countdown all new players. teamswitch will rebalance on every switch to the red or blue team, if applicable.
+
     """
     def __init__(self):
         """
         default constructor, adds the plugin hooks and initializes variables used
         """
         super().__init__()
+        self.last_new_player_id = None
+        if not self.game or self.game.state != "in_progress":
+            self.balanced_player_steam_ids = []
+        else:
+            teams = self.teams()
+            self.balanced_player_steam_ids = [player.steam_id for player in teams["red"] + teams["blue"]]
+
+        self.set_cvar_once("qlx_rebalanceMethod", "countdown")
+
+        self.rebalance_method = self.get_cvar("qlx_rebalanceMethod")
+
+        self.add_hook("team_switch_attempt", self.handle_team_switch_attempt)
         self.add_hook("round_countdown", self.handle_round_countdown)
         self.add_hook("round_start", self.handle_round_start, priority=minqlx.PRI_LOWEST)
         self.add_hook("game_countdown", self.handle_game_countdown, priority=minqlx.PRI_LOWEST)
 
-        self.previous_round_player_steam_ids = []
+        self.add_command("rebalanceMethod", self.cmd_rebalance_method, permission=3, usage="[countdown|teamsswitch]")
 
-        self.plugin_version = "{} Version: {}".format(self.name, "v0.0.1")
+        self.plugin_version = "{} Version: {}".format(self.name, "v0.0.2")
         self.logger.info(self.plugin_version)
+
+    def handle_team_switch_attempt(self, player, old, new):
+        """
+        Handles the case where a player switches from spectators to "red", "blue", or "any" team, and
+        the resulting teams would be sub-optimal balanced.
+
+        :param player: the player that attempted the switch
+        :param old: the old team of the switching player
+        :param new: the new team that swicthting player would be put onto
+
+        :return minqlx.RET_NONE if the team switch should be allowed or minqlx.RET_STOP_EVENT if the switch should not
+        be allowed, and we put the player on a better-suited teamö.
+        """
+        if not self.game:
+            return minqlx.RET_NONE
+
+        if self.game.state != "in_progress":
+            return minqlx.RET_NONE
+
+        if old not in ["spectator"] or new not in ['red', 'blue', 'any']:
+            return minqlx.RET_NONE
+
+        if self.rebalance_method != "teamswitch":
+            return minqlx.RET_NONE
+
+        if "balance" not in self.plugins:
+            Plugin.msg("^1balance^7 plugin not loaded, ^1auto rebalance^7 not possible.")
+            return minqlx.RET_NONE
+
+        gametype = self.game.type_short
+        if gametype not in SUPPORTED_GAMETYPES:
+            return minqlx.RET_NONE
+
+        teams = self.teams()
+        if len(teams["red"]) == len(teams["blue"]):
+            self.last_new_player_id = player.steam_id
+            return minqlx.RET_NONE
+
+        if not self.last_new_player_id:
+            return minqlx.RET_NONE
+
+        last_new_player = Plugin.player(self.last_new_player_id)
+        if not last_new_player:
+            return minqlx.RET_NONE
+
+        other_than_last_players_team = self.other_team(last_new_player.team)
+        new_player_team = teams[other_than_last_players_team].copy()
+        new_player_team.append(player)
+        last_player_avg = self.team_average(teams[last_new_player.team], gametype)
+        new_player_avg = self.team_average(new_player_team, gametype)
+        proposed_diff = abs(last_player_avg - new_player_avg)
+
+        alternative_team_a = [player for player in teams[last_new_player.team] if player != last_new_player]
+        alternative_team_a.append(player)
+        alternative_new_avg = self.team_average(alternative_team_a, gametype)
+        alternative_team_b = teams[other_than_last_players_team].copy()
+        alternative_team_b.append(last_new_player)
+        alternative_last_avg = self.team_average(alternative_team_b, gametype)
+        alternative_diff = abs(alternative_last_avg - alternative_new_avg)
+
+        self.last_new_player_id = None
+        if proposed_diff > alternative_diff:
+            last_new_player.put(other_than_last_players_team)
+            if new in [last_new_player.team]:
+                return minqlx.RET_NONE
+            player.put(last_new_player.team)
+            return minqlx.RET_STOP_EVENT
+
+        if new not in ["any", other_than_last_players_team]:
+            player.put(other_than_last_players_team)
+            return minqlx.RET_STOP_EVENT
+
+        return minqlx.RET_NONE
+
+    def other_team(self, team):
+        if team == "red":
+            return "blue"
+        return "red"
 
     def handle_round_countdown(self, roundnumber):
         """
@@ -46,20 +141,26 @@ class auto_rebalance(minqlx.Plugin):
 
         :param roundnumber: the round number that is about to start
         """
+        if self.rebalance_method != "countdown":
+            return
+
         if "balance" not in self.plugins:
             Plugin.msg("^1balance^7 plugin not loaded, ^1auto rebalance^7 not possible.")
-            return
+            return minqlx.RET_NONE
 
         gametype = self.game.type_short
         if gametype not in SUPPORTED_GAMETYPES:
-            return
+            return minqlx.RET_NONE
+
+        if roundnumber == 1:
+            return minqlx.RET_NONE
 
         teams = self.teams()
 
         new_red_players = [player for player in teams["red"]
-                           if player.steam_id not in self.previous_round_player_steam_ids]
+                           if player.steam_id not in self.balanced_player_steam_ids]
         new_blue_players = [player for player in teams["blue"]
-                            if player.steam_id not in self.previous_round_player_steam_ids]
+                            if player.steam_id not in self.balanced_player_steam_ids]
 
         if len(new_red_players) + len(new_blue_players) < 2:
             return
@@ -80,21 +181,35 @@ class auto_rebalance(minqlx.Plugin):
             Plugin.msg("New team members already on optimal teams. Nothing to rebalance")
             return
 
+        switches = []
+
         while switch:
             p1 = switch[0][0]
             p2 = switch[0][1]
-            self.switch(p1, p2)
+            switches.append(switch[0])
             teams["blue"].append(p1)
             teams["red"].append(p2)
             teams["blue"].remove(p2)
             teams["red"].remove(p1)
             new_players = {'red': [player for player in teams["red"]
-                                   if player.steam_id not in self.previous_round_player_steam_ids
-                                   and player != last_player],
+                                   if player.steam_id not in self.balanced_player_steam_ids and
+                                   player != last_player],
                            'blue': [player for player in teams["blue"]
-                                    if player.steam_id not in self.previous_round_player_steam_ids
-                                    and player != last_player]}
+                                    if player.steam_id not in self.balanced_player_steam_ids and
+                                    player != last_player]}
             switch = self.suggest_switch(teams, new_players, gametype)
+
+        self.perform_switches(switches)
+
+    @minqlx.thread
+    def perform_switches(self, switches):
+        """
+        pereforms collected player switches in its own thread to not interfere with other game logic
+
+        :param switches: a list of player pairs that should be switched with each other
+        """
+        for switch in switches:
+            self.switch(switch[0], switch[1])
 
     def suggest_switch(self, teams, new_players, gametype):
         """Suggest a switch based on average team ratings."""
@@ -139,10 +254,20 @@ class auto_rebalance(minqlx.Plugin):
         Remembers the steam ids of all players at round startup
         """
         teams = Plugin.teams()
-        self.previous_round_player_steam_ids = [player.steam_id for player in teams["red"] + teams["blue"]]
+        self.balanced_player_steam_ids = [player.steam_id for player in teams["red"] + teams["blue"]]
+        self.last_new_player_id = None
 
     def handle_game_countdown(self):
         """
         When a new game starts, the steam ids of the previous rounds are re-initialized.
         """
-        self.previous_round_player_steam_ids = []
+        self.balanced_player_steam_ids = []
+
+    def cmd_rebalance_method(self, player, msg, channel):
+        if len(msg) < 2 or msg[1] not in ["countdown", "teamswitch"]:
+            player.tell("Current rebalance method is: ^4{}^7".format(self.rebalance_method))
+            return minqlx.RET_USAGE
+        else:
+            self.rebalance_method = msg[1]
+            Plugin.set_cvar("qlx_rebalanceMethod", self.rebalance_method)
+            player.tell("Rebalance method set to: ^4{}^7".format(self.rebalance_method))
--- src/unittest/python/auto_rebalance_tests.py
+++ src/unittest/python/auto_rebalance_tests.py
@@ -16,6 +16,7 @@ class AutoRebalanceTests(unittest.TestCase):
         setup_game_in_progress()
         connected_players()
         self.plugin = auto_rebalance()
+        self.plugin.rebalance_method = "countdown"
 
     def tearDown(self):
         unstub()
@@ -23,6 +24,207 @@ class AutoRebalanceTests(unittest.TestCase):
     def setup_no_balance_plugin(self):
         if "balance" in self.plugin._loaded_plugins:
             del self.plugin._loaded_plugins["balance"]
+
+    def test_handle_team_switch_attempt_no_game_running(self):
+        setup_no_game()
+        self.plugin.rebalance_method = "teamswitch"
+        player = fake_player(42, "Fake Player")
+        connected_players(player)
+
+        return_code = self.plugin.handle_team_switch_attempt(player, "spectator", "any")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+
+    def test_handle_team_switch_attempt_game_in_warmup(self):
+        setup_game_in_warmup()
+        self.plugin.rebalance_method = "teamswitch"
+        player = fake_player(42, "Fake Player")
+        connected_players(player)
+
+        return_code = self.plugin.handle_team_switch_attempt(player, "spectator", "any")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+
+    def test_handle_team_switch_attempt_not_from_spectator(self):
+        self.plugin.rebalance_method = "teamswitch"
+        player = fake_player(42, "Fake Player")
+        connected_players(player)
+
+        return_code = self.plugin.handle_team_switch_attempt(player, "blue", "red")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+
+    def test_handle_team_switch_attempt_to_spectator(self):
+        self.plugin.rebalance_method = "teamswitch"
+        player = fake_player(42, "Fake Player")
+        connected_players(player)
+
+        return_code = self.plugin.handle_team_switch_attempt(player, "blue", "spectator")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+
+    def test_handle_team_switch_attempt_rebalance_not_set_for_teamswitch_events(self):
+        self.plugin.rebalance_method = "countdown"
+        player = fake_player(42, "Fake Player")
+        connected_players(player)
+
+        return_code = self.plugin.handle_team_switch_attempt(player, "spectator", "red")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+
+    def test_handle_team_switch_attempt_no_balance_plugin_loaded(self):
+        self.setup_no_balance_plugin()
+        self.plugin.rebalance_method = "teamswitch"
+        player = fake_player(42, "Fake Player")
+        connected_players(player)
+
+        return_code = self.plugin.handle_team_switch_attempt(player, "spectator", "red")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+        assert_plugin_sent_to_console(matches(".*not possible.*"))
+
+    def test_handle_team_switch_attempt_wrong_gasmetype(self):
+        setup_game_in_progress(game_type="rr")
+        self.setup_balance_ratings([])
+        self.plugin.rebalance_method = "teamswitch"
+        player = fake_player(42, "Fake Player")
+        connected_players(player)
+
+        return_code = self.plugin.handle_team_switch_attempt(player, "spectator", "red")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+
+    def test_handle_team_switch_attempt_third_player_joins(self):
+        self.plugin.rebalance_method = "teamswitch"
+        red_player = fake_player(123, "Red Player", "red")
+        blue_player = fake_player(246, "Blue Player", "blue")
+        new_player = fake_player(42, "New Player", "spectator")
+        connected_players(red_player, blue_player, new_player)
+
+        self.setup_previous_players(red_player, blue_player)
+        self.setup_balance_ratings([(red_player, 1200), (blue_player, 1200), (new_player, 1200)])
+
+        return_code = self.plugin.handle_team_switch_attempt(new_player, "spectator", "red")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+        assert_that(self.plugin.last_new_player_id, is_(new_player.steam_id))
+
+    def test_handle_team_switch_attempt_fourth_player_joins_no_last_id_set(self):
+        self.plugin.rebalance_method = "teamswitch"
+        red_player = fake_player(123, "Red Player", "red")
+        blue_player = fake_player(246, "Blue Player", "blue")
+        new_red_player = fake_player(41, "New Red Player", "red")
+        new_player = fake_player(42, "New Player", "spectator")
+        connected_players(red_player, blue_player, new_red_player, new_player)
+
+        self.setup_previous_players(red_player, blue_player, new_red_player)
+        self.setup_balance_ratings([(red_player, 1200), (blue_player, 1200),
+                                    (new_red_player, 1200), (new_player, 1200)])
+
+        return_code = self.plugin.handle_team_switch_attempt(new_player, "spectator", "red")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+
+    def test_handle_team_switch_attempt_fourth_player_joins_last_player_disconnected(self):
+        self.plugin.rebalance_method = "teamswitch"
+        red_player = fake_player(123, "Red Player", "red")
+        blue_player = fake_player(246, "Blue Player", "blue")
+        new_red_player = fake_player(41, "New Red Player", "red")
+        new_player = fake_player(42, "New Player", "spectator")
+        connected_players(red_player, blue_player, new_red_player, new_player)
+
+        self.setup_previous_players(red_player, blue_player, new_red_player)
+        self.setup_balance_ratings([(red_player, 1200), (blue_player, 1200),
+                                    (new_red_player, 1200), (new_player, 1200)])
+        self.plugin.last_new_player_id = 666
+
+        return_code = self.plugin.handle_team_switch_attempt(new_player, "spectator", "red")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+
+    def test_handle_team_switch_attempt_fourth_player_joins_teams_already_balanced(self):
+        self.plugin.rebalance_method = "teamswitch"
+        red_player = fake_player(123, "Red Player", "red")
+        blue_player = fake_player(246, "Blue Player", "blue")
+        new_red_player = fake_player(41, "New Red Player", "red")
+        new_player = fake_player(42, "New Player", "spectator")
+        connected_players(red_player, blue_player, new_red_player, new_player)
+
+        self.setup_previous_players(red_player, blue_player, new_red_player)
+        self.setup_balance_ratings([(red_player, 1200), (blue_player, 1200),
+                                    (new_red_player, 1200), (new_player, 1200)])
+        self.plugin.last_new_player_id = new_red_player.steam_id
+
+        return_code = self.plugin.handle_team_switch_attempt(new_player, "spectator", "blue")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+        assert_that(self.plugin.last_new_player_id, is_(None))
+
+    def test_handle_team_switch_attempt_fourth_player_joins_wrong_teams(self):
+        self.plugin.rebalance_method = "teamswitch"
+        red_player = fake_player(123, "Red Player", "red")
+        blue_player = fake_player(246, "Blue Player", "blue")
+        new_red_player = fake_player(41, "New Red Player", "red")
+        new_player = fake_player(42, "New Player", "spectator")
+        connected_players(red_player, blue_player, new_red_player, new_player)
+
+        self.setup_previous_players(red_player, blue_player, new_red_player)
+        self.setup_balance_ratings([(red_player, 1200), (blue_player, 1200),
+                                    (new_red_player, 1200), (new_player, 1200)])
+        self.plugin.last_new_player_id = new_red_player.steam_id
+
+        return_code = self.plugin.handle_team_switch_attempt(new_player, "spectator", "red")
+
+        assert_that(return_code, is_(minqlx.RET_STOP_EVENT))
+        assert_player_was_put_on(new_player, "blue")
+        assert_that(self.plugin.last_new_player_id, is_(None))
+
+    def test_handle_team_switch_attempt_fourth_player_joins_would_lead_to_less_optimal_teamss(self):
+        self.plugin.rebalance_method = "teamswitch"
+        red_player = fake_player(123, "Red Player", "red")
+        blue_player = fake_player(246, "Blue Player", "blue")
+        new_blue_player = fake_player(41, "New Red Player", "blue")
+        new_player = fake_player(42, "New Player", "spectator")
+        connected_players(red_player, blue_player, new_blue_player, new_player)
+
+        self.setup_previous_players(red_player, blue_player, new_blue_player)
+        self.setup_balance_ratings([(red_player, 1200), (blue_player, 1400),
+                                    (new_blue_player, 1400), (new_player, 1200)])
+        self.plugin.last_new_player_id = new_blue_player.steam_id
+
+        return_code = self.plugin.handle_team_switch_attempt(new_player, "spectator", "red")
+
+        assert_that(return_code, is_(minqlx.RET_STOP_EVENT))
+        assert_player_was_put_on(new_player, "blue")
+        assert_player_was_put_on(new_blue_player, "red")
+        assert_that(self.plugin.last_new_player_id, is_(None))
+
+    def test_handle_team_switch_attempt_fourth_player_joins_not_to_better_balanced_team(self):
+        self.plugin.rebalance_method = "teamswitch"
+        red_player = fake_player(123, "Red Player", "red")
+        blue_player = fake_player(246, "Blue Player", "blue")
+        new_blue_player = fake_player(41, "New Red Player", "blue")
+        new_player = fake_player(42, "New Player", "spectator")
+        connected_players(red_player, blue_player, new_blue_player, new_player)
+
+        self.setup_previous_players(red_player, blue_player, new_blue_player)
+        self.setup_balance_ratings([(red_player, 1200), (blue_player, 1400),
+                                    (new_blue_player, 1400), (new_player, 1200)])
+        self.plugin.last_new_player_id = new_blue_player.steam_id
+
+        return_code = self.plugin.handle_team_switch_attempt(new_player, "spectator", "blue")
+
+        assert_that(return_code, is_(minqlx.RET_NONE))
+        assert_player_was_put_on(new_player, "blue", times=0)
+        assert_player_was_put_on(new_blue_player, "red")
+        assert_that(self.plugin.last_new_player_id, is_(None))
+
+    def test_handle_round_countdown_rebalance_method_during_team_switch(self):
+        self.plugin.rebalance_method = "teamswitch"
+
+        self.plugin.handle_round_countdown(5)
+
+        assert_plugin_sent_to_console(any, times=0)
 
     def test_handle_round_countdown_prints_message_when_balance_plugin_not_loaded(self):
         self.setup_no_balance_plugin()
@@ -46,8 +248,15 @@ class AutoRebalanceTests(unittest.TestCase):
 
         assert_plugin_sent_to_console(any, times=0)
 
+    def test_handle_round_countdown_does_nothing_for_first_round(self):
+        self.setup_balance_ratings([])
+
+        self.plugin.handle_round_countdown(1)
+
+        assert_plugin_sent_to_console(any, times=0)
+
     def setup_previous_players(self, *players):
-        self.plugin.previous_round_player_steam_ids = [player.steam_id for player in players]
+        self.plugin.balanced_player_steam_ids = [player.steam_id for player in players]
 
     def test_handle_round_countdown_nothing_to_rebalance(self):
         red_player1 = fake_player(123, "Red Player1", "red")
@@ -148,12 +357,51 @@ class AutoRebalanceTests(unittest.TestCase):
 
         self.plugin.handle_round_start(3)
 
-        assert_that(self.plugin.previous_round_player_steam_ids,
+        assert_that(self.plugin.balanced_player_steam_ids,
                     is_([player.steam_id for player in [red_player1, red_player2, blue_player1, blue_player2]]))
 
     def test_handle_game_countdown_resets_remembered_steam_ids(self):
-        self.plugin.previous_round_player_steam_ids = [123, 456, 246, 975]
+        self.plugin.balanced_player_steam_ids = [123, 456, 246, 975]
 
         self.plugin.handle_game_countdown()
 
-        assert_that(self.plugin.previous_round_player_steam_ids, is_([]))
+        assert_that(self.plugin.balanced_player_steam_ids, is_([]))
+
+    def test_cmd_rebalance_method_shows_usage_for_too_few_parameters(self):
+        player = fake_player(123, "Fake Player")
+
+        return_code = self.plugin.cmd_rebalance_method(player, ["!rebalanceMethod"], None)
+
+        assert_that(return_code, is_(minqlx.RET_USAGE))
+        assert_player_was_told(player, "Current rebalance method is: ^4countdown^7")
+
+    def test_cmd_rebalance_method_shows_usage_for_too_many_parameters(self):
+        self.plugin.rebalance_method = "teamswitch"
+        player = fake_player(123, "Fake Player")
+
+        return_code = self.plugin.cmd_rebalance_method(player, ["!rebalanceMethod", "asdf", "qwerty"], None)
+
+        assert_that(return_code, is_(minqlx.RET_USAGE))
+        assert_player_was_told(player, "Current rebalance method is: ^4teamswitch^7")
+
+    def test_cmd_rebalance_method_shows_usage_for_invalid_parameter(self):
+        self.plugin.rebalance_method = "teamswitch"
+        player = fake_player(123, "Fake Player")
+
+        return_code = self.plugin.cmd_rebalance_method(player, ["!rebalanceMethod", "asdf"], None)
+
+        assert_that(return_code, is_(minqlx.RET_USAGE))
+        assert_player_was_told(player, "Current rebalance method is: ^4teamswitch^7")
+
+    def test_cmd_rebalance_method_set_to_another_value(self):
+        spy2(Plugin.set_cvar)
+        when2(Plugin.set_cvar, any, any).thenReturn(None)
+
+        self.plugin.rebalance_method = "teamswitch"
+        player = fake_player(123, "Fake Player")
+
+        self.plugin.cmd_rebalance_method(player, ["!rebalanceMethod", "countdown"], None)
+
+        assert_player_was_told(player, "Rebalance method set to: ^4countdown^7")
+        assert_that(self.plugin.rebalance_method, is_("countdown"))
+        verify(Plugin).set_cvar("qlx_rebalanceMethod", "countdown")
