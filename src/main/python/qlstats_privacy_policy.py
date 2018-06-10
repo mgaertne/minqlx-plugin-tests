ee930e013751e64ec082373881a86e3615300c6a
Markus Gärtner <markus.gaertner@it-agile.de>
2018-06-11 00:16:39 +0200

    replaced player.msg with player.tell, as player has no msg

--- src/main/python/qlstats_privacy_policy.py
+++ src/main/python/qlstats_privacy_policy.py
@@ -63,7 +63,7 @@ class qlstats_privacy_policy(minqlx.Plugin):
 
             player_info = self.plugins["balance"].player_info
             if player.steam_id not in player_info:
-                player.msg("We couldn't fetch your ratings, yet. You will not be able to join, until we did.")
+                player.tell("We couldn't fetch your ratings, yet. You will not be able to join, until we did.")
                 return minqlx.RET_STOP_ALL
             if player_info[player.steam_id]["privacy"] not in self.allowed_privacy:
                 self.msg("{}^7, you're not allowed to join any team "
