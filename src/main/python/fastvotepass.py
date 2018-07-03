62a8b34516fe9ac06e1aa23e0e73b8a231c70e20
Markus Gärtner <markus.gaertner@it-agile.de>
2018-07-03 14:55:40 +0200

    first version of fast vote pass with threshold passing as its only option

---
+++ src/main/python/fastvotepass.py
@@ -0,0 +1,88 @@
+import minqlx
+
+from minqlx import Plugin
+
+
+class fastvotepass(minqlx.Plugin):
+    """
+    This plugin modifies the default vote pass or fail behavior with customizeable logic.
+
+    Uses:
+    * qlx_fastvoteTypes (default: "map, kick") Vote types this plugin will care about.
+    * qlx_fastvoteStrategy (default: "threshold") The strategy used to process faster pass/fail votes.
+    Currently available options: threshold
+
+    For the threshold strategy, you may set/modify these additional cvars:
+    * qlx_fastvoteThresholdFastPassDiff (default: 6) passes the callvote at this yes-no difference
+    * qlx_fastvoteThresholdFastFailDiff (default: 5) fails the callvote at this no-yes difference
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.set_cvar_once("qlx_fastvoteTypes", "map, kick")
+        self.set_cvar_once("qlx_fastvoteStrategy", "threshold")
+
+        self.fastvote_types = self.get_cvar("qlx_fastvoteTypes", list)
+        strategy = self.get_cvar("qlx_fastvoteStrategy", str)
+        self.fastvote_strategy = self.resolve_strategy_for_fastvote(strategy)
+
+        self.add_hook("vote", self.process_vote, priority=minqlx.PRI_LOWEST)
+        self.add_hook("vote_called", self.handle_vote, priority=minqlx.PRI_LOWEST)
+        self.add_hook("vote_ended", self.handle_vote_ended, priority=minqlx.PRI_LOWEST)
+
+        self.track_vote = False
+
+    def resolve_strategy_for_fastvote(self, strategy):
+        return ThresholdFastVoteStrategy()
+
+    def handle_vote(self, player, vote, args):
+        if vote.lower() in self.fastvote_types:
+            self.track_vote = True
+
+    def handle_vote_ended(self, votes, vote, args, passed):
+        self.track_vote = False
+
+    def process_vote(self, player, yes):
+        if not self.track_vote:
+            return
+
+        if not Plugin.is_vote_active():
+            self.track_vote = False
+            return
+
+        votes = Plugin.current_vote_count()
+
+        if not votes:
+            return
+
+        yes_votes = votes[0] + 1 if yes else votes[0]
+        no_votes = votes[1] if yes else votes[1] + 1
+
+        eval_result = self.fastvote_strategy.evaluate_votes(yes_votes, no_votes)
+        if eval_result is None:
+            return
+
+        self.track_vote = False
+        Plugin.force_vote(eval_result)
+
+
+class ThresholdFastVoteStrategy:
+
+    def __init__(self):
+        Plugin.set_cvar_once("qlx_fastvoteThresholdFastPassDiff", 6)
+        Plugin.set_cvar_once("qlx_fastvoteThresholdFastFailDiff", 5)
+
+        self.threshold_fast_pass_diff = Plugin.get_cvar("qlx_fastvoteThresholdFastPassDiff", int)
+        self.threshold_fast_fail_diff = Plugin.get_cvar("qlx_fastvoteThresholdFastFailDiff", int)
+
+    def evaluate_votes(self, yes_votes, no_votes):
+        diff = yes_votes - no_votes
+
+        if diff >= self.threshold_fast_pass_diff:
+            return True
+
+        if diff <= -self.threshold_fast_fail_diff:
+            return False
+
+        return None
---
+++ src/unittest/python/fastvotepass_tests.py
@@ -0,0 +1,122 @@
+from minqlx_plugin_test import *
+
+import unittest
+
+from mockito import *
+from mockito.matchers import *
+from hamcrest import *
+
+from fastvotepass import *
+
+
+class FastVotePassTests(unittest.TestCase):
+
+    def setUp(self):
+        setup_plugin()
+        setup_cvars({
+            "qlx_fastvoteTypes": (["map"], list),
+            "qlx_fastvoteStrategy": ("threshold", str),
+            "qlx_fastvoteThresholdFastPassDiff": (6, int),
+            "qlx_fastvoteThresholdFastFailDiff": (5, int)
+        })
+        spy2(Plugin.force_vote)
+        spy2(Plugin.current_vote_count)
+
+        self.plugin = fastvotepass()
+
+    def tearDown(self):
+        unstub()
+
+    def setup_vote_in_progress(self):
+        spy2(Plugin.is_vote_active)
+        when2(Plugin.is_vote_active).thenReturn(True)
+
+    def test_handle_vote_map_vote_called_tracks_votes(self):
+        self.plugin.track_vote = False
+
+        self.plugin.handle_vote(fake_player(123, "Any Player"), "map", "thunderstruck")
+
+        assert_that(self.plugin.track_vote, is_(True))
+
+    def test_handle_vote_map_vote_called_with_mixed_cases(self):
+        self.plugin.track_vote = False
+
+        self.plugin.handle_vote(fake_player(123, "Any Player"), "mAP", "theatreofpain")
+
+        assert_that(self.plugin.track_vote, is_(True))
+
+    def test_handle_vote_kick_player_is_not_tracked(self):
+        self.plugin.track_vote = False
+
+        self.plugin.handle_vote(fake_player(123, "Any Player"), "kick", "Fake Player")
+
+        assert_that(self.plugin.track_vote, is_(False))
+
+    def test_handle_vote_ended_resets_tracking(self):
+        self.plugin.track_vote = True
+
+        self.plugin.handle_vote_ended([3, 2], "map", "campgrounds", True)
+
+        assert_that(self.plugin.track_vote, is_(False))
+
+    def test_process_vote_with_no_active_vote_running(self):
+        when2(Plugin.is_vote_active).thenReturn(False)
+        self.plugin.track_vote = True
+
+        self.plugin.process_vote(fake_player(123, "Any Player"), True)
+
+        assert_that(self.plugin.track_vote, is_(False))
+        verify(Plugin, times=0).force_vote(any)
+
+    def current_vote_count_is(self, number_yes, number_no):
+        when2(Plugin.current_vote_count).thenReturn((number_yes, number_no))
+
+    def test_player_votes_yes_total_vote_count_does_not_meet_threshold(self):
+        self.setup_vote_in_progress()
+        self.plugin.track_vote = True
+        self.current_vote_count_is(1, 2)
+
+        self.plugin.process_vote(fake_player(123, "Any Player"), True)
+
+        assert_that(self.plugin.track_vote, is_(True))
+        verify(Plugin, times=0).force_vote(any)
+
+    def test_player_votes_yes_and_hits_vote_threshold(self):
+        self.setup_vote_in_progress()
+        self.plugin.track_vote = True
+        self.current_vote_count_is(6, 1)
+
+        self.plugin.process_vote(fake_player(123, "Any Player"), True)
+
+        assert_that(self.plugin.track_vote, is_(False))
+        verify(Plugin).force_vote(True)
+
+    def test_player_votes_no_and_does_not_meet_threashold(self):
+        self.setup_vote_in_progress()
+        self.plugin.track_vote = True
+        self.current_vote_count_is(1, 2)
+
+        self.plugin.process_vote(fake_player(123, "Any Player"), False)
+
+        assert_that(self.plugin.track_vote, is_(True))
+        verify(Plugin, times=0).force_vote(any)
+
+    def test_player_votes_no_and_hits_vote_threshold(self):
+        self.setup_vote_in_progress()
+        self.plugin.track_vote = True
+        self.current_vote_count_is(1, 5)
+
+        self.plugin.process_vote(fake_player(123, "Any Player"), False)
+
+        assert_that(self.plugin.track_vote, is_(False))
+        verify(Plugin).force_vote(False)
+
+    def test_player_votes_on_an_untracked_vote(self):
+        self.setup_vote_in_progress()
+        self.plugin.track_vote = False
+        self.current_vote_count_is(1, 5)
+
+        self.plugin.process_vote(fake_player(123, "Any Player"), False)
+
+        assert_that(self.plugin.track_vote, is_(False))
+        verify(Plugin, times=0).force_vote(any)
