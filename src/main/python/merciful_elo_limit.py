c328b9cbcca7d8c8d35c7fe5e350b3d1581438dc
Markus Gärtner <markus.gaertner@it-agile.de>
2018-12-27 14:05:08 +0100

    added a new plugin with tests for merciful elo limits

---
+++ src/main/python/merciful_elo_limit.py
@@ -0,0 +1,191 @@
+import minqlx
+from minqlx import Plugin, RET_NONE, CHAT_CHANNEL
+
+import time
+
+FREE_GAMES_KEY = "minqlx:players:{}:minelo:freegames"
+ABOVE_GAMES_KEY = "minqlx:players:{}:minelo:abovegames"
+
+SUPPORTED_GAMETYPES = ("ca", "ctf", "dom", "ft", "tdm", "duel", "ffa")
+
+
+class merciful_elo_limit(Plugin):
+    """
+    Uses:
+    * qlx_merciful_minelo (default: 800) The minimum elo for free games. Players below this limit are tracked.
+    * qlx_mercifulelo_freegames (default: 10) The amount of free games a player below the minimum elo will be able to
+    play
+    * qlx_mercifulelo_abovegames (default: 10) The amount of games a player needs to complete in a row with at least
+    minimum elo before stopping to track her.
+    * qlx_mercifulelo_daysbanned (default: 30) The number of days a low elo player gets banned after using up his
+    free games
+    """
+    def __init__(self):
+        super().__init__()
+        self.set_cvar_once("qlx_mercifulelo_minelo", 800)
+        self.set_cvar_once("qlx_mercifulelo_freegames", 10)
+        self.set_cvar_once("qlx_mercifulelo_abovegames", 10)
+        self.set_cvar_once("qlx_mercifulelo_daysbanned", 30)
+
+        self.min_elo = self.get_cvar("qlx_mercifulelo_minelo", int)
+        self.free_games = self.get_cvar("qlx_mercifulelo_freegames", int)
+        self.above_games = self.get_cvar("qlx_mercifulelo_abovegames", int)
+        self.banned_days = self.get_cvar("qlx_mercifulelo_daysbanned", int)
+
+        self.tracked_player_sids = []
+
+        self.add_hook("map", self.handle_map_change)
+        self.add_hook("player_connect", self.handle_player_connect)
+        self.add_hook("round_countdown", self.handle_round_countdown)
+        self.add_hook("round_start", self.handle_round_start)
+
+    def handle_map_change(self, mapname, factory):
+        self.tracked_player_sids = []
+        self.fetch_elos_of_players(Plugin.players())
+
+    def handle_player_connect(self, player):
+        self.fetch_elos_of_players([player])
+
+    def fetch_elos_of_players(self, players):
+        if not self.game:
+            return
+
+        gametype = self.game.type_short
+        if gametype not in SUPPORTED_GAMETYPES:
+            return
+
+        if 'balance' not in Plugin._loaded_plugins:
+            self.logger.warning("Balance plugin not found. Merciful elo limits just work with the elos "
+                                "from the balance plugin")
+            return
+
+        balance_plugin = Plugin._loaded_plugins['balance']
+
+        player_ratings = dict([(p.steam_id, gametype) for p in players])
+        balance_plugin.add_request(player_ratings, self.callback_ratings, CHAT_CHANNEL)
+
+    def handle_round_countdown(self, round_number):
+        if not self.game:
+            return RET_NONE
+
+        teams = Plugin.teams()
+        self.fetch_elos_of_players(teams["red"] + teams["blue"])
+
+    def callback_ratings(self, players, channel):
+        @minqlx.next_frame
+        def ban_player(player, duration, msg):
+            minqlx.COMMANDS.handle_input(DummyOwner(self.logger),
+                                         "!ban {} {} {}".format(player.steam_id, duration, msg),
+                                         DummyChannel(self.logger))
+
+        if not self.game:
+            return
+
+        teams = self.teams()
+        for player in teams["red"] + teams["blue"]:
+            elo = self.elo_for_player(player)
+            if elo is None:
+                continue
+
+            if elo < self.min_elo:
+                free_games_played = int(self.db.get(FREE_GAMES_KEY.format(player.steam_id)))
+                if free_games_played > self.free_games:
+                    ban_player(player, "30 days",
+                               "Automatically banned after using up {} free games".format(self.free_games))
+                    self.db.delete(ABOVE_GAMES_KEY.format(player.steam_id))
+                    self.db.delete(FREE_GAMES_KEY.format(player.steam_id))
+                else:
+                    self.warn_lowelo_player(player)
+
+    def elo_for_player(self, player):
+        if not self.game:
+            return None
+
+        if 'balance' not in Plugin._loaded_plugins:
+            self.logger.warning("Balance plugin not found. Merciful elo limits just work with the elos "
+                                "from the balance plugin")
+            return None
+        balance_plugin = Plugin._loaded_plugins['balance']
+        ratings = balance_plugin.ratings
+
+        gametype = self.game.type_short
+        if gametype not in SUPPORTED_GAMETYPES:
+            return None
+
+        return ratings[player.steam_id][gametype]["elo"]
+
+    def warn_lowelo_player(self, player):
+        remaining_matches = self.free_games - self.db.get(FREE_GAMES_KEY.format(player.steam_id))
+        self.blink2(player, "^1Skill warning, read console! ^3{} ^1matches left.".format(remaining_matches))
+        player.tell(
+            "{}, this is a ^1Skill Warning! ^7Your qlstats.net glicko is below {}. You have ^3{} ^7of {} free matches "
+            "left, before server will automatically ban you for {} days"
+            .format(player.clean_name, self.min_elo, remaining_matches, self.free_games, self.banned_days))
+        player.tell(
+            "You will get {} new free matches after the {} days ban. Please improve your skill! "
+            "Tip: Practice the Elevate and Accelerate training from the Quake Live menu and some Free For All on other "
+            "servers."
+            .format(self.free_games, self.banned_days))
+
+    @minqlx.thread
+    def blink2(self, player, message, count=12, interval=.12):
+        @minqlx.next_frame
+        def logic(target, msg):
+            target.center_print("^3{}".format(msg))
+
+        for msg_number in range(count):
+            logic(player, message)
+            time.sleep(interval)
+            logic(player, "")
+            time.sleep(interval)
+
+    def handle_round_start(self, round_number):
+        teams = Plugin.teams()
+        for player in teams["red"] + teams["blue"]:
+            if player.steam_id in self.tracked_player_sids:
+                continue
+            elo = self.elo_for_player(player)
+            if elo is None:
+                continue
+
+            if elo < self.min_elo:
+                self.db.incr(FREE_GAMES_KEY.format(player.steam_id))
+                self.tracked_player_sids.append(player.steam_id)
+
+                if self.db.exists(ABOVE_GAMES_KEY.format(player.steam_id)):
+                    self.db.delete(ABOVE_GAMES_KEY.format(player.steam_id))
+                return
+
+            if self.db.exists(FREE_GAMES_KEY.format(player.steam_id)):
+                self.tracked_player_sids.append(player.steam_id)
+                self.db.incr(ABOVE_GAMES_KEY.format(player.steam_id))
+                above_games = int(self.db.get(ABOVE_GAMES_KEY.format(player.steam_id)))
+                if above_games > self.above_games:
+                    self.db.delete(ABOVE_GAMES_KEY.format(player.steam_id))
+                    self.db.delete(FREE_GAMES_KEY.format(player.steam_id))
+
+
+class DummyChannel(minqlx.AbstractChannel):
+    def __init__(self, logger):
+        super().__init__("merciful_elo_limit")
+        self.logger = logger
+
+    def reply(self, msg):
+        self.logger.info(msg)
+
+
+class DummyOwner(minqlx.AbstractDummyPlayer):
+    def __init__(self, logger):
+        super().__init__(name="Owner")
+        self.logger = logger
+
+    @property
+    def steam_id(self):
+        return minqlx.owner()
+
+    @property
+    def channel(self):
+        return DummyChannel(self.logger)
+
+    def tell(self, msg):
+        self.logger.info(msg)
---
+++ src/unittest/python/merciful_elo_limit_tests.py
@@ -0,0 +1,332 @@
+from minqlx_plugin_test import *
+
+import logging
+import unittest
+
+from mockito import *
+from mockito.matchers import *
+from hamcrest import *
+
+from redis import Redis
+
+from merciful_elo_limit import *
+
+
+class MercifulEloLimitTests(unittest.TestCase):
+
+    def setUp(self):
+        setup_plugin()
+        setup_cvars({
+            "qlx_mercifulelo_minelo": (800, int),
+            "qlx_mercifulelo_freegames": (10, int),
+            "qlx_mercifulelo_abovegames": (10, int),
+            "qlx_mercifulelo_daysbanned": (30, int)
+        })
+        spy2(minqlx.get_cvar)
+        when(minqlx).get_cvar("qlx_owner").thenReturn("42")
+
+        setup_game_in_progress()
+        self.plugin = merciful_elo_limit()
+
+        self.plugin.database = Redis
+        self.db = mock(Redis)
+        self.plugin._db_instance = self.db
+
+        when(self.db).__getitem__(any).thenReturn("42")
+
+    def tearDown(self):
+        unstub()
+
+    def setup_balance_ratings(self, player_elos):
+        gametype = None
+        if len(player_elos) > 0:
+            gametype = self.plugin.game.type_short
+        ratings = {}
+        for player, elo in player_elos:
+            ratings[player.steam_id] = {gametype: {'elo': elo}}
+        self.plugin._loaded_plugins["balance"] = mock({'ratings': ratings})
+
+    def setup_no_balance_plugin(self):
+        if "balance" in self.plugin._loaded_plugins:
+            del self.plugin._loaded_plugins["balance"]
+
+    def test_handle_map_change_resets_tracked_player_ids(self):
+        connected_players()
+        self.setup_balance_ratings([])
+        self.plugin.tracked_player_sids = [123, 455]
+
+        self.plugin.handle_map_change("campgrounds", "ca")
+
+        assert_that(self.plugin.tracked_player_sids, is_([]))
+
+    def test_handle_map_change_fetches_elos_of_connected_players(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({(player1, 900), (player2, 1200)})
+
+        self.plugin.handle_map_change("thunderstruck", "ca")
+
+        verify(self.plugin._loaded_plugins["balance"]).add_request(
+            {player1.steam_id: 'ca', player2.steam_id: 'ca'},
+            self.plugin.callback_ratings, minqlx.CHAT_CHANNEL
+        )
+
+    def test_handle_player_connect_fetches_elo_of_connecting_player(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connecting_player = fake_player(789, "Connecting Player")
+        connected_players(player1, player2, connecting_player)
+        self.setup_balance_ratings({(player1, 900), (player2, 1200), (connecting_player, 1542)})
+
+        self.plugin.handle_player_connect(connecting_player)
+
+        verify(self.plugin._loaded_plugins["balance"]).add_request(
+            {connecting_player.steam_id: 'ca'},
+            self.plugin.callback_ratings, minqlx.CHAT_CHANNEL
+        )
+
+    def test_fetch_elos_of_players_with_no_game_setup(self):
+        setup_no_game()
+        self.setup_balance_ratings({})
+
+        self.plugin.fetch_elos_of_players([])
+
+        verify(self.plugin._loaded_plugins["balance"], times=0).add_request(any, any, any)
+
+    def test_fetch_elos_of_players_with_unsupported_gametype(self):
+        setup_game_in_progress("unsupported")
+        self.setup_balance_ratings({})
+
+        self.plugin.fetch_elos_of_players([])
+
+        verify(self.plugin._loaded_plugins["balance"], times=0).add_request(any, any, any)
+
+    def test_fetch_elos_of_player_with_no_balance_plugin(self):
+        mocked_logger = mock(spec=logging.Logger)
+        spy2(minqlx.get_logger)
+        when(minqlx).get_logger(self.plugin).thenReturn(mocked_logger)
+        self.setup_no_balance_plugin()
+
+        self.plugin.fetch_elos_of_players([])
+
+        verify(mocked_logger).warning(matches("Balance plugin not found.*"))
+
+    def test_handle_round_countdown_with_no_game(self):
+        setup_no_game()
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        player3 = fake_player(789, "Speccing Player", team="spectator")
+        connected_players(player1, player2, player3)
+        self.setup_balance_ratings({})
+
+        self.plugin.handle_round_countdown(1)
+
+        verify(self.plugin._loaded_plugins["balance"], times=0).add_request(any, any, any)
+
+    def test_handle_round_countdown_fetches_elos_of_players_in_teams(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        player3 = fake_player(789, "Speccing Player", team="spectator")
+        connected_players(player1, player2, player3)
+        self.setup_balance_ratings({(player1, 900), (player2, 1200), (player3, 1600)})
+
+        self.plugin.handle_round_countdown(4)
+
+        verify(self.plugin._loaded_plugins["balance"]).add_request(
+            {player1.steam_id: 'ca', player2.steam_id: 'ca'},
+            self.plugin.callback_ratings, minqlx.CHAT_CHANNEL
+        )
+
+    def test_callback_ratings_with_no_game_running(self):
+        setup_no_game()
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        player3 = fake_player(789, "Speccing Player", team="spectator")
+        connected_players(player1, player2, player3)
+        self.setup_balance_ratings({})
+
+        self.plugin.callback_ratings([], minqlx.CHAT_CHANNEL)
+
+        verify(self.db, times=0).get(any)
+
+    def test_callback_ratings_with_unsupported_game_type(self):
+        setup_game_in_progress("unsupported")
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        player3 = fake_player(789, "Speccing Player", team="spectator")
+        connected_players(player1, player2, player3)
+        self.setup_balance_ratings({})
+
+        self.plugin.callback_ratings([], minqlx.CHAT_CHANNEL)
+
+        verify(self.db, times=0).get(any)
+
+    def test_callback_ratings_warns_low_elo_player(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({(player1, 900), (player2, 799)})
+
+        patch(minqlx.next_frame, lambda func: func)
+        patch(minqlx.thread, lambda func: func)
+        patch(time.sleep, lambda int: None)
+        when(self.db).get(any).thenReturn(2)
+
+        self.plugin.callback_ratings([player1, player2], minqlx.CHAT_CHANNEL)
+
+        verify(player2, times=12).center_print(matches(".*Skill warning.*8.*matches left.*"))
+        verify(player2).tell(matches(".*Skill Warning.*qlstats.*below.*800.*8.*of 10 free matches.*"))
+
+    def test_callback_ratings_bans_low_elo_players_that_used_up_their_free_games(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({(player1, 900), (player2, 799)})
+
+        when(self.db).get(any).thenReturn(11)
+        spy2(minqlx.COMMANDS.handle_input)
+        when2(minqlx.COMMANDS.handle_input, any, any, any).thenReturn(None)
+
+        patch(minqlx.PlayerInfo, lambda *args: mock(spec=minqlx.PlayerInfo))
+        patch(minqlx.next_frame, lambda func: func)
+
+        when(self.db).delete(any).thenReturn(None)
+
+        self.plugin.callback_ratings([player1, player2], minqlx.CHAT_CHANNEL)
+
+        verify(minqlx.COMMANDS).handle_input(any, any, any)
+        verify(self.db).delete("minqlx:players:{}:minelo:abovegames".format(player2.steam_id))
+        verify(self.db).delete("minqlx:players:{}:minelo:freegames".format(player2.steam_id))
+
+    def test_handle_round_start_increases_free_games_for_untracked_player(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({(player1, 900), (player2, 799)})
+
+        when(self.db).get(any).thenReturn(3)
+        when(self.db).delete(any).thenReturn(None)
+        when(self.db).exists(any).thenReturn(False)
+        when(self.db).incr(any).thenReturn(None)
+
+        self.plugin.handle_round_start(1)
+
+        verify(self.db).incr("minqlx:players:{}:minelo:freegames".format(player2.steam_id))
+
+    def test_handle_round_start_starts_tracking_for_low_elo_player(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({(player1, 900), (player2, 799)})
+
+        when(self.db).get(any).thenReturn(3)
+        when(self.db).delete(any).thenReturn(None)
+        when(self.db).exists(any).thenReturn(False)
+        when(self.db).incr(any).thenReturn(None)
+
+        self.plugin.handle_round_start(1)
+
+        assert_that(self.plugin.tracked_player_sids, has_item(player2.steam_id))
+
+    def test_handle_round_start_resets_above_games_for_low_elo_player(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({(player1, 900), (player2, 799)})
+
+        when(self.db).get(any).thenReturn(3)
+        when(self.db).delete(any).thenReturn(None)
+        when(self.db).exists(any).thenReturn(True)
+        when(self.db).incr(any).thenReturn(None)
+
+        self.plugin.handle_round_start(1)
+
+        verify(self.db).delete("minqlx:players:{}:minelo:abovegames".format(player2.steam_id))
+
+    def test_handle_round_start_increases_above_games_for_free_games_player(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({(player1, 900), (player2, 801)})
+
+        when(self.db).get(any).thenReturn(3)
+        when(self.db).delete(any).thenReturn(None)
+        when(self.db).exists(any).thenReturn(True)
+        when(self.db).incr(any).thenReturn(None)
+
+        self.plugin.handle_round_start(1)
+
+        verify(self.db).incr("minqlx:players:{}:minelo:abovegames".format(player2.steam_id))
+
+    def test_handle_round_start_starts_tracking_of_above_elo_players_for_free_games_player(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({(player1, 900), (player2, 801)})
+
+        when(self.db).get(any).thenReturn(3)
+        when(self.db).delete(any).thenReturn(None)
+        when(self.db).exists(any).thenReturn(True)
+        when(self.db).incr(any).thenReturn(None)
+
+        self.plugin.handle_round_start(1)
+
+        assert_that(self.plugin.tracked_player_sids, has_item(player2.steam_id))
+
+    def test_handle_round_start_removes_minelo_db_entries_for_above_elo_player(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({(player1, 900), (player2, 801)})
+
+        when(self.db).get(any).thenReturn(11)
+        when(self.db).delete(any).thenReturn(None)
+        when(self.db).exists(any).thenReturn(True)
+        when(self.db).incr(any).thenReturn(None)
+
+        self.plugin.handle_round_start(1)
+
+        verify(self.db).delete("minqlx:players:{}:minelo:freegames".format(player2.steam_id))
+        verify(self.db).delete("minqlx:players:{}:minelo:abovegames".format(player2.steam_id))
+
+    def test_handle_round_start_skips_already_tracked_player(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.plugin.tracked_player_sids.append(player2.steam_id)
+        self.setup_balance_ratings({(player1, 900), (player2, 799)})
+
+        when(self.db).get(any).thenReturn(3)
+        when(self.db).delete(any).thenReturn(None)
+        when(self.db).exists(any).thenReturn(False)
+        when(self.db).incr(any).thenReturn(None)
+
+        self.plugin.handle_round_start(1)
+
+        verify(self.db, times=0).delete(any)
+        verify(self.db, times=0).delete(any)
+
+    def test_handle_round_start_with_unsupported_gametype(self):
+        setup_game_in_progress("unsupported")
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        self.setup_balance_ratings({})
+
+        self.plugin.handle_round_start(2)
+
+        verify(self.plugin._loaded_plugins["balance"], times=0).add_request(any, any, any)
+
+    def test_handle_round_start_with_no_balance_plugin(self):
+        player1 = fake_player(123, "Fake Player1", team="red")
+        player2 = fake_player(456, "Fake Player2", team="blue")
+        connected_players(player1, player2)
+        mocked_logger = mock(spec=logging.Logger)
+        spy2(minqlx.get_logger)
+        when(minqlx).get_logger(self.plugin).thenReturn(mocked_logger)
+        self.setup_no_balance_plugin()
+
+        self.plugin.handle_round_start(5)
+
+        verify(mocked_logger, atleast=1).warning(matches("Balance plugin not found.*"))
