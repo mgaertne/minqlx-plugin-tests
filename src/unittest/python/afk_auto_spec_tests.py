e1c6ccd361d03a2e5abecea05eb2209617bde4dc
Markus Gärtner <markus.gaertner@it-agile.de>
2019-02-10 15:25:23 +0100

    added player_inactive_warning trigger as well to unittest

--- src/unittest/python/afk_auto_spec_tests.py
+++ src/unittest/python/afk_auto_spec_tests.py
@@ -14,7 +14,8 @@ class AfkAutoSpecTests(unittest.TestCase):
     def setUp(self):
         setup_plugin()
         setup_cvars({
-            "qlx_autoSpecWarmup": (False, bool)
+            "qlx_autoSpecWarmup": (False, bool),
+            "g_inactivityWarning": (10, int)
         })
         setup_game_in_progress()
 
@@ -100,3 +101,73 @@ class AfkAutoSpecTests(unittest.TestCase):
         self.plugin.handle_player_inactive(idle_player)
 
         assert_player_was_put_on(idle_player, "spectator", times=0)
+
+    def test_handle_player_inactive_warning_player_warns_player(self):
+        idle_player = fake_player(42, "Idle Player", "red")
+        connected_players(idle_player)
+
+        self.plugin.handle_player_inactive_warning(idle_player)
+
+        assert_player_received_center_print(idle_player, matches(".*Knock! Knock!.*"))
+
+    def test_handle_player_inactive_warning_stops_further_event_processing(self):
+        idle_player = fake_player(42, "Idle Player", "red")
+        connected_players(idle_player)
+
+        returned = self.plugin.handle_player_inactive_warning(idle_player)
+
+        assert_that(returned, is_(minqlx.RET_STOP_ALL))
+
+    def test_handle_player_inactive_warning_with_no_game_does_nothing(self):
+        setup_no_game()
+        idle_player = fake_player(42, "Idle Player", "red")
+        connected_players(idle_player)
+
+        self.plugin.handle_player_inactive_warning(idle_player)
+
+        assert_player_received_center_print(idle_player, any, times=0)
+
+    def test_handle_player_inactive_warning_during_warmup_does_nothing(self):
+        setup_game_in_warmup()
+        idle_player = fake_player(42, "Idle Player", "red")
+        connected_players(idle_player)
+
+        self.plugin.handle_player_inactive_warning(idle_player)
+
+        assert_player_received_center_print(idle_player, any, times=0)
+
+    def test_handle_player_inactive_warning_during_warmup_with_warmup_spec_enabled(self):
+        setup_game_in_warmup()
+        self.plugin.spec_warmup = True
+        idle_player = fake_player(42, "Idle Player", "red")
+        connected_players(idle_player)
+
+        self.plugin.handle_player_inactive_warning(idle_player)
+
+        assert_player_received_center_print(idle_player, matches(".*Knock! Knock!.*"))
+
+    def test_handle_player_inactive_warning_on_team_free_in_ca(self):
+        idle_player = fake_player(42, "Idle Player", "free")
+        connected_players(idle_player)
+
+        self.plugin.handle_player_inactive_warning(idle_player)
+
+        assert_player_received_center_print(idle_player, any, times=0)
+
+    def test_handle_player_inactive_warning_on_team_free_in_ffa(self):
+        setup_game_in_progress("ffa")
+        idle_player = fake_player(42, "Idle Player", "free")
+        connected_players(idle_player)
+
+        self.plugin.handle_player_inactive_warning(idle_player)
+
+        assert_player_received_center_print(idle_player, matches(".*Knock! Knock!.*"))
+
+    def test_handle_player_inactive_warning_on_team_spectator_in_ffa(self):
+        setup_game_in_progress("ffa")
+        idle_player = fake_player(42, "Idle Player", "spectator")
+        connected_players(idle_player)
+
+        self.plugin.handle_player_inactive_warning(idle_player)
+
+        assert_player_received_center_print(idle_player, any, times=0)
