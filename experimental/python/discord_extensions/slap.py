63ff1b51182b07a734f6202d828036ff129cb733
mgaertne <mgaertne@gmail.com>
2022-10-10 00:18:51 +0200

    added some more slap variations and checks for own slaps and slaps to the bot.

--- experimental/python/discord_extensions/slap.py
+++ experimental/python/discord_extensions/slap.py
@@ -1,3 +1,5 @@
+import random
+
 # noinspection PyPackageRequirements
 from discord import app_commands, Interaction, Member
 # noinspection PyPackageRequirements
@@ -7,7 +9,21 @@ from discord.ext.commands import Bot
 @app_commands.context_menu(name="slap")
 @app_commands.guild_only()
 async def slap(interaction: Interaction, member: Member):
-    await interaction.response.send_message(f"_{interaction.user.mention} slaps {member.mention} with a large trout._")
+    if member.id == interaction.client.user.id:
+        await interaction.response.send_message(f"_slaps {interaction.user.mention} with a large revenge trout._")
+
+    if member.id == interaction.user.id:
+        await interaction.response.send_message(f"_slaps {interaction.user.mention} slaps himself for his stupidity._")
+
+    slaps = [
+        f"_{interaction.user.mention} slaps {member.mention} with a large trout._",
+        f"_{interaction.user.mention} slaps a large trout with {member.mention}._",
+        f"_a large trout slaps {interaction.user.mention} with a {member.mention}._",
+        f"_{interaction.user.mention} is trouted by {member.mention} with a large slap._",
+        f"_{interaction.user.mention} gives {member.mention} a high five. In the face. With a chair._",
+    ]
+
+    await interaction.response.send_message(random.choice(slaps))
 
 
 async def setup(bot: Bot):
