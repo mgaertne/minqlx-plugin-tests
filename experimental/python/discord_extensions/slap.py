d38112d36ebf0d97dbf4d77bc1f328330216f62b
mgaertne <mgaertne@gmail.com>
2022-11-14 00:46:27 +0100

    now slaps are also sent to quake

--- experimental/python/discord_extensions/slap.py
+++ experimental/python/discord_extensions/slap.py
@@ -1,29 +1,55 @@
 import random
 
 # noinspection PyPackageRequirements
-from discord import app_commands, Interaction, Member
+from discord import app_commands, Interaction, Member, PartialMessageable, InteractionMessage
 # noinspection PyPackageRequirements
 from discord.ext.commands import Bot
+
+from minqlx import Plugin
 
 
 @app_commands.context_menu(name="slap")
 @app_commands.guild_only()
 async def slap(interaction: Interaction, member: Member):
+    if interaction.client.user is None:
+        await send_to_discord_and_quake(interaction, f"_{member.mention} is slapped from the hidden.")
+        return
+
     if member.id == interaction.client.user.id:
-        await interaction.response.send_message(f"_slaps {interaction.user.mention} with a large revenge trout._")
+        await send_to_discord_and_quake(interaction, f"_slaps {interaction.user.mention} with a large revenge trout._")
+        return
 
     if member.id == interaction.user.id:
-        await interaction.response.send_message(f"_slaps {interaction.user.mention} slaps himself for his stupidity._")
+        await send_to_discord_and_quake(interaction,
+                                        f"_{interaction.user.mention} slaps himself for his stupidity._")
+        return
 
     slaps = [
         f"_{interaction.user.mention} slaps {member.mention} with a large trout._",
         f"_{interaction.user.mention} slaps a large trout with {member.mention}._",
-        f"_a large trout slaps {interaction.user.mention} with a {member.mention}._",
-        f"_{interaction.user.mention} is trouted by {member.mention} with a large slap._",
+        f"_a large trout slaps {member.mention} with a {interaction.user.mention}._",
+        f"_{member.mention} is trouted by {interaction.user.mention} with a large slap._",
         f"_{interaction.user.mention} gives {member.mention} a high five. In the face. With a chair._",
     ]
 
-    await interaction.response.send_message(random.choice(slaps))
+    await send_to_discord_and_quake(interaction, random.choice(slaps))
+
+
+async def send_to_discord_and_quake(interaction: Interaction, message: str) -> None:
+    await interaction.response.send_message(message)
+
+    interaction_response: InteractionMessage = await interaction.original_response()
+    mentioned_users = interaction_response.mentions
+    quake_message = message.lstrip("_").rstrip("_")
+    for user in mentioned_users:
+        quake_message = quake_message.replace(user.mention, user.display_name)
+
+    discord_message_prefix: str = Plugin.get_cvar("qlx_discordMessagePrefix") or "[DISCORD]"
+    if interaction.channel is None or isinstance(interaction.channel, PartialMessageable):
+        Plugin.msg(f"{discord_message_prefix}^2 {quake_message}")
+        return
+
+    Plugin.msg(f"{discord_message_prefix} ^5#{interaction.channel.name}^7:^2 {quake_message}")
 
 
 async def setup(bot: Bot):
