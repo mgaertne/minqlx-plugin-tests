58348a7fd9493b6f77291d266e347f717ca994ca
Markus Gärtner <markus.gaertner@it-agile.de>
2022-11-29 01:14:29 +0100

    another test file migrated to pytest

--- src/unittest/python/test_custom_modes_vote.py
+++ src/unittest/python/test_custom_modes_vote.py
@@ -1,31 +1,31 @@
-import unittest
+import pytest
 
-from minqlx_plugin_test import setup_plugin, setup_cvars, fake_player, assert_plugin_sent_to_console
+from minqlx_plugin_test import fake_player, assert_plugin_sent_to_console, setup_cvars
 
 from mockito import spy2, unstub, verify  # type: ignore
 from mockito.matchers import matches, any_  # type: ignore
-from hamcrest import assert_that, is_
+from hamcrest import assert_that, equal_to
 
 import minqlx
 from minqlx import Plugin
 from custom_modes_vote import custom_modes_vote
 
 
-class CustomModesVoteTests(unittest.TestCase):
+class TestCustomModesVote:
 
-    def setUp(self):
-        setup_plugin()
-
+    def setup_method(self):
         setup_cvars({
             "qlx_modeVoteNewMapDefault": "vql",
         })
+
         spy2(Plugin.callvote)
         spy2(minqlx.console_command)
         spy2(minqlx.client_command)
 
         self.plugin = custom_modes_vote()
 
-    def tearDown(self):
+    @staticmethod
+    def teardown_method():
         unstub()
 
     def test_handle_map_change(self):
@@ -33,7 +33,7 @@ class CustomModesVoteTests(unittest.TestCase):
 
         self.plugin.handle_map_change("campgrounds", "ca")
 
-        assert_that(self.plugin.mode, is_("vql"))
+        assert_that(self.plugin.mode, equal_to("vql"))
 
     def test_handle_map_change_already_in_default_mode(self):
         self.plugin.mode = "vql"
@@ -55,7 +55,7 @@ class CustomModesVoteTests(unittest.TestCase):
 
         return_code = self.plugin.handle_vote_called(voting_player, "mode", "pql")
 
-        assert_that(return_code, is_(minqlx.RET_STOP_ALL))
+        assert_that(return_code, equal_to(minqlx.RET_STOP_ALL))
         verify(Plugin).callvote("mode pql", "mode pql")
         verify(minqlx).client_command(voting_player.id, "vote yes")
         assert_plugin_sent_to_console(matches(r".*called a vote\."))
@@ -65,7 +65,7 @@ class CustomModesVoteTests(unittest.TestCase):
 
         return_code = self.plugin.handle_vote_called(voting_player, "mode", "unavailable")
 
-        assert_that(return_code, is_(minqlx.RET_NONE))
+        assert_that(return_code, equal_to(minqlx.RET_NONE))
         verify(Plugin, times=0).callvote(any_, any_)
         verify(minqlx, times=0).client_command(any_, any_)
         assert_plugin_sent_to_console(any_, times=0)
@@ -75,7 +75,7 @@ class CustomModesVoteTests(unittest.TestCase):
 
         return_code = self.plugin.handle_vote_called(voting_player, "mode", "vql")
 
-        assert_that(return_code, is_(minqlx.RET_STOP_ALL))
+        assert_that(return_code, equal_to(minqlx.RET_STOP_ALL))
         verify(Plugin, times=0).callvote(any_, any_)
         verify(minqlx, times=0).client_command(any_, any_)
         assert_plugin_sent_to_console(any_, times=0)
@@ -85,7 +85,7 @@ class CustomModesVoteTests(unittest.TestCase):
 
         return_code = self.plugin.handle_vote_called(voting_player, "map", "campgrounds ca")
 
-        assert_that(return_code, is_(minqlx.RET_NONE))
+        assert_that(return_code, equal_to(minqlx.RET_NONE))
         verify(Plugin, times=0).callvote(any_, any_)
         verify(minqlx, times=0).client_command(any_, any_)
         assert_plugin_sent_to_console(any_, times=0)
@@ -93,12 +93,12 @@ class CustomModesVoteTests(unittest.TestCase):
     def test_handle_vote_ended_vote_passed(self):
         self.plugin.handle_vote_ended(None, "mode", "pql", True)
 
-        assert_that(self.plugin.mode, is_("pql"))
+        assert_that(self.plugin.mode, equal_to("pql"))
 
     def test_handle_vote_ended_vote_failed(self):
         self.plugin.handle_vote_ended(None, "mode", "pql", False)
 
-        assert_that(self.plugin.mode, is_("vql"))
+        assert_that(self.plugin.mode, equal_to("vql"))
 
     def test_handle_vote_ended_unavailable_mode_for_this_plugin(self):
         self.plugin.handle_vote_ended(None, "mode", "unavailable", True)
@@ -113,20 +113,20 @@ class CustomModesVoteTests(unittest.TestCase):
     def test_cmd_switch_mode_no_mode_given(self):
         return_code = self.plugin.cmd_switch_mode(fake_player(123, "Admin"), ["!mode"], None)
 
-        assert_that(return_code, is_(minqlx.RET_USAGE))
+        assert_that(return_code, equal_to(minqlx.RET_USAGE))
 
     def test_cmd_switch_mode_too_many_parameters_given(self):
         return_code = self.plugin.cmd_switch_mode(fake_player(123, "Admin"), ["!mode", "asdf", "qwertz"], None)
 
-        assert_that(return_code, is_(minqlx.RET_USAGE))
+        assert_that(return_code, equal_to(minqlx.RET_USAGE))
 
     def test_cmd_switch_mode_unavailable_mode(self):
         return_code = self.plugin.cmd_switch_mode(fake_player(123, "Admin"), ["!mode", "unavailable"], None)
 
-        assert_that(return_code, is_(minqlx.RET_USAGE))
+        assert_that(return_code, equal_to(minqlx.RET_USAGE))
 
     def test_cmd_switch_mode_to_available_mode(self):
         return_code = self.plugin.cmd_switch_mode(fake_player(123, "Admin"), ["!mode", "pql"], None)
 
-        assert_that(return_code, is_(minqlx.RET_NONE))
-        assert_that(self.plugin.mode, is_("pql"))
+        assert_that(return_code, equal_to(minqlx.RET_NONE))
+        assert_that(self.plugin.mode, equal_to("pql"))
