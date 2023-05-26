de82692d90303f0b7517a9c92ed11b505a35ce9e
mgaertne <mgaertne@gmail.com>
2023-05-26 13:47:42 +0200

    some minor fixes to make the plugin work. Need to remember that minqlx.Player has no play_sound function.

--- src/experimental/team_shots.py
+++ src/experimental/team_shots.py
@@ -1,3 +1,4 @@
+import itertools
 from ast import literal_eval
 
 from minqlx import Plugin
@@ -7,12 +8,13 @@ from minqlx import Plugin
 class team_shots(Plugin):
     def __init__(self):
         super().__init__()
-        
-        self.set_cvar_once("qlx_teamShots_minHealth", "5")
+
+        self.set_cvar_once("qlx_teamShots_minHealth", "1")
         self.set_cvar_once("qlx_teamShots_punishments", "{}")
 
-        self.min_health = self.get_cvar("qlx_teamShots_minHealth", int) or 5        
-        self.punishment_lookup = literal_eval(self.get_cvar("qlx_teamShots_punushments") or "{}")
+        self.min_health = self.get_cvar("qlx_teamShots_minHealth", int) or 1
+        punishment_cvar_value = self.get_cvar("qlx_teamShots_punishments") or "{}"
+        self.punishment_lookup = literal_eval(punishment_cvar_value) or {}
 
         self.team_damages = {}
         self.team_shots = {}
@@ -27,10 +29,37 @@ class team_shots(Plugin):
         self.team_shots = {}
 
     def handle_game_end(self, _data):
-        self.logger.debug("Overall team damages:")
-        for steam_id, damage in self.team_damages.items():
-            player = self.player(steam_id)
-            self.logger.debug(f"team dmg: {player.clean_name}: {damage} dmg ({self.team_shots[player.steam_id]} shots)")
+        if len(self.team_damages) == 0:
+            return
+
+        grouped_team_damages = itertools.groupby(
+            sorted(self.team_damages, key=self.team_damages.get, reverse=True),  # type: ignore
+            key=self.team_damages.get,
+        )
+        grouped_team_damages_dict = {
+            team_dmg: list(steam_ids) for team_dmg, steam_ids in grouped_team_damages
+        }
+
+        self.msg("^7Top 3 team damage inflicter:")
+        extra = 0
+        for counter, (team_damage, steam_ids) in enumerate(
+            grouped_team_damages_dict.items(), start=1
+        ):
+            if counter + extra > 3:
+                break
+
+            prefix = f"^5{counter + extra:2}^7."
+            extra += max(0, len(steam_ids) - 1)
+
+            for steam_id in steam_ids:
+                player = self.player(steam_id)
+                if player is None:
+                    continue
+                self.msg(
+                    f"  {prefix} {player.name}^7 (^5{team_damage} team damage^7, "
+                    f"^5{self.team_shots[steam_id]} shots^7)"
+                )
+                prefix = "   "
 
     def handle_damage_event(self, target, attacker, dmg, dflags, means_of_death):
         if not self.game:
@@ -53,7 +82,12 @@ class team_shots(Plugin):
             if punishment > 0:
                 if attacker.health > self.min_health:
                     attacker.health -= punishment
+                self.play_sound("sound/feedback/hit_teammate.ogg", player=attacker)
                 attacker.center_print("^1watch your fire!")
 
-            self.team_damages[attacker.steam_id] = self.team_damages.get(attacker.steam_id, 0) + dmg
-            self.team_shots[attacker.steam_id] = self.team_shots.get(attacker.steam_id, 0) + 1
+            self.team_damages[attacker.steam_id] = (
+                self.team_damages.get(attacker.steam_id, 0) + dmg
+            )
+            self.team_shots[attacker.steam_id] = (
+                self.team_shots.get(attacker.steam_id, 0) + 1
+            )
