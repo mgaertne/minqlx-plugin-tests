11b2d4c0ce48556e2f24c851d1b35281914095e2
mgaertne <mgaertne@gmail.com>
2023-05-26 19:43:15 +0200

    some final beta testing polishing before the first try

--- src/experimental/chat_fraggers.py
+++ src/experimental/chat_fraggers.py
@@ -1,10 +1,42 @@
 from minqlx import Plugin
 
 
+def warn_player(player):
+    player.center_print("^1Shoot Chatters = Insta-Ban")
+
+
+def punish_player(player):
+    player.armor = 0
+    player.health = 1
+    player.weapons(
+        g=False,
+        mg=False,
+        sg=False,
+        gl=False,
+        rl=False,
+        lg=False,
+        rg=False,
+        pg=False,
+        bfg=False,
+        gh=False,
+        ng=False,
+        pl=False,
+        cg=False,
+        hmg=False,
+    )
+    player.weapon(15)
+    player.center_print("^1You have been warned!")
+
+
 # noinspection PyPep8Naming
 class chat_fraggers(Plugin):
     def __init__(self):
         super().__init__()
+
+        self.set_cvar_once("qlx_chatFraggers_punishment_threshold", "250")
+        self.punishment_threshold = (
+            self.get_cvar("qlx_chatFraggers_punishment_threshold", int) or 250
+        )
 
         self.chat_tracker = {}
 
@@ -32,46 +64,48 @@ class chat_fraggers(Plugin):
         if target.team == attacker.team:
             return
 
-        if hasattr(target.state, "is_chatting") and target.state.is_chatting:
-            if attacker.steam_id not in self.chat_tracker:
-                self.chat_tracker[attacker.steam_id] = {}
-            self.chat_tracker[attacker.steam_id][target.steam_id] += (
-                self.chat_tracker[attacker.steam_id].get(target.steam_id, 0) + dmg
-            )
-            attacker.center_print("^1Shoot Chatters = Insta-Ban")
+        if not hasattr(target.state, "is_chatting") or not target.state.is_chatting:
+            return
+
+        if attacker.steam_id not in self.chat_tracker:
+            self.chat_tracker[attacker.steam_id] = {}
+        self.chat_tracker[attacker.steam_id][target.steam_id] = (
+            self.chat_tracker[attacker.steam_id].get(target.steam_id, 0) + dmg
+        )
+
+        if (
+            self.chat_tracker[attacker.steam_id][target.steam_id]
+            < self.punishment_threshold
+        ):
+            warn_player(attacker)
+            return
+
+        punish_player(attacker)
 
     def handle_kill(self, victim, killer, _data):
-        if not victim.state.is_chatting:
+        if not self.game:
+            return
+
+        if self.game.state != "in_progress":
+            return
+
+        if not hasattr(victim.state, "is_chatting") or not victim.state.is_chatting:
             return
 
         if killer is None:
             return
-        
+
         if killer.steam_id not in self.chat_tracker:
             return
 
         if victim.steam_id not in self.chat_tracker[killer.steam_id]:
             return
 
-        if self.chat_tracker[killer.steam_id][victim.steam_id] < 300:
+        if (
+            self.chat_tracker[killer.steam_id][victim.steam_id]
+            < self.punishment_threshold
+        ):
+            warn_player(killer)
             return
 
-        killer.armor = 0
-        killer.health = 1
-        killer.weapons(
-            g=False,
-            mg=False,
-            sg=False,
-            gl=False,
-            rl=False,
-            lg=False,
-            rg=False,
-            pg=False,
-            bfg=False,
-            gh=False,
-            ng=False,
-            pl=False,
-            cg=False,
-            hmg=False,
-        )
-        killer.center_print("^1You have been warned!")
+        punish_player(killer)
