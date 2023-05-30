b5e624a87f48454b6389dccef3003057bf0cc908
mgaertne <mgaertne@gmail.com>
2023-05-30 15:46:33 +0200

    externalized the center print texts, and added a couple more options

--- src/experimental/team_shots.py
+++ src/experimental/team_shots.py
@@ -1,5 +1,7 @@
 import itertools
+import time
 from ast import literal_eval
+from operator import itemgetter
 
 from minqlx import Plugin
 
@@ -11,13 +13,21 @@ class team_shots(Plugin):
 
         self.set_cvar_once("qlx_teamShots_minHealth", "1")
         self.set_cvar_once("qlx_teamShots_punishments", "{}")
+        self.set_cvar_once("qlx_teamShots_min_damage", "15")
+        self.set_cvar_once("qlx_teamShots_warning_text", "^1watch your fire!")
+        self.set_cvar_once("qlx_teamShots_filtered_means_of_death", "18")
 
         self.min_health = self.get_cvar("qlx_teamShots_minHealth", int) or 1
         punishment_cvar_value = self.get_cvar("qlx_teamShots_punishments") or "{}"
         self.punishment_lookup = literal_eval(punishment_cvar_value) or {}
+        self.min_damage = self.get_cvar("qlx_teamShots_min_damage", int) or 15
+        self.warning_text = (
+                self.get_cvar("qlx_teamShots_warning_text") or "^1watch your fire!"
+        )
+        filtered_mod_strings = self.get_cvar("qlx_teamShots_filtered_means_of_death", list) or ["18"]
+        self.filtered_means_of_death = [int(entry) for entry in filtered_mod_strings]
 
         self.team_damages = {}
-        self.team_shots = {}
 
         self.add_hook("game_countdown", self.handle_game_countdown)
         self.add_hook("game_end", self.handle_game_end)
@@ -26,15 +36,20 @@ class team_shots(Plugin):
 
     def handle_game_countdown(self):
         self.team_damages = {}
-        self.team_shots = {}
 
     def handle_game_end(self, _data):
         if len(self.team_damages) == 0:
             return
 
+        team_damages = {}
+        for steam_id in self.team_damages:
+            team_damages[steam_id] = sum(
+                [dmg for _, dmg in self.team_damages[steam_id]]
+            )
+
         grouped_team_damages = itertools.groupby(
-            sorted(self.team_damages, key=self.team_damages.get, reverse=True),  # type: ignore
-            key=self.team_damages.get,
+            sorted(team_damages, key=team_damages.get, reverse=True),  # type: ignore
+            key=team_damages.get,
         )
         grouped_team_damages_dict = {
             team_dmg: list(steam_ids) for team_dmg, steam_ids in grouped_team_damages
@@ -43,7 +58,7 @@ class team_shots(Plugin):
         self.msg("^7Top 3 team damage inflicter:")
         extra = 0
         for counter, (team_damage, steam_ids) in enumerate(
-            grouped_team_damages_dict.items(), start=1
+                grouped_team_damages_dict.items(), start=1
         ):
             if counter + extra > 3:
                 break
@@ -57,11 +72,13 @@ class team_shots(Plugin):
                     continue
                 self.msg(
                     f"  {prefix} {player.name}^7 (^5{team_damage} team damage^7, "
-                    f"^5{self.team_shots[steam_id]} shots^7)"
+                    f"^5{len(self.team_damages[steam_id])} shots^7)"
                 )
                 prefix = "   "
 
     def handle_damage_event(self, target, attacker, dmg, dflags, means_of_death):
+        timestamp = int(time.time() * 1000)
+
         if not self.game:
             return
 
@@ -77,17 +94,29 @@ class team_shots(Plugin):
         if target.team != attacker.team:
             return
 
-        if dflags & 16 == 0:
-            punishment = self.punishment_lookup.get(means_of_death, 0)
-            if punishment > 0:
-                if attacker.health > self.min_health:
-                    attacker.health -= punishment
-                self.play_sound("sound/feedback/hit_teammate.ogg", player=attacker)
-                attacker.center_print("^1watch your fire!")
+        if dmg < self.min_damage:
+            return
 
-            self.team_damages[attacker.steam_id] = (
-                self.team_damages.get(attacker.steam_id, 0) + dmg
-            )
-            self.team_shots[attacker.steam_id] = (
-                self.team_shots.get(attacker.steam_id, 0) + 1
-            )
+        if means_of_death in self.filtered_means_of_death:
+            return
+
+        if dflags & 16 != 0:
+            return
+
+        punishment = self.punishment_lookup.get(means_of_death, 0)
+        if punishment > 0:
+            if attacker.health > self.min_health:
+                attacker.health -= punishment
+            self.play_sound("sound/feedback/hit_teammate.ogg", player=attacker)
+
+            if (
+                    attacker.steam_id in self.team_damages
+                    and timestamp
+                    - max(self.team_damages[attacker.steam_id], key=itemgetter(1))[0]
+                    < 2000
+            ):
+                attacker.center_print(self.warning_text)
+
+        if attacker.steam_id not in self.team_damages:
+            self.team_damages[attacker.steam_id] = []
+        self.team_damages[attacker.steam_id].append((timestamp, dmg))
