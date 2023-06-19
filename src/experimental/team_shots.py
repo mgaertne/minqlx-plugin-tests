084b71e6d32e08bcded4098642cf5aa735f9d6db
mgaertne <mgaertne@gmail.com>
2023-06-20 00:44:37 +0200

    delegated game_end results to weird_stats completely. Also checking for target.health <= 0 now.

--- src/experimental/team_shots.py
+++ src/experimental/team_shots.py
@@ -1,4 +1,3 @@
-import itertools
 import time
 from ast import literal_eval
 from operator import itemgetter
@@ -22,59 +21,21 @@ class team_shots(Plugin):
         self.punishment_lookup = literal_eval(punishment_cvar_value) or {}
         self.min_damage = self.get_cvar("qlx_teamShots_min_damage", int) or 15
         self.warning_text = (
-                self.get_cvar("qlx_teamShots_warning_text") or "^1watch your fire!"
+            self.get_cvar("qlx_teamShots_warning_text") or "^1watch your fire!"
         )
-        filtered_mod_strings = self.get_cvar("qlx_teamShots_filtered_means_of_death", list) or ["18"]
+        filtered_mod_strings = self.get_cvar(
+            "qlx_teamShots_filtered_means_of_death", list
+        ) or ["18"]
         self.filtered_means_of_death = [int(entry) for entry in filtered_mod_strings]
 
         self.team_damages = {}
 
         self.add_hook("game_countdown", self.handle_game_countdown)
-        self.add_hook("game_end", self.handle_game_end)
 
         self.add_hook("damage", self.handle_damage_event)
 
     def handle_game_countdown(self):
         self.team_damages = {}
-
-    def handle_game_end(self, _data):
-        if len(self.team_damages) == 0:
-            return
-
-        team_damages = {}
-        for steam_id in self.team_damages:
-            team_damages[steam_id] = sum(
-                [dmg for _, dmg in self.team_damages[steam_id]]
-            )
-
-        grouped_team_damages = itertools.groupby(
-            sorted(team_damages, key=team_damages.get, reverse=True),  # type: ignore
-            key=team_damages.get,
-        )
-        grouped_team_damages_dict = {
-            team_dmg: list(steam_ids) for team_dmg, steam_ids in grouped_team_damages
-        }
-
-        self.msg("^7Top 3 team damage inflicter:")
-        extra = 0
-        for counter, (team_damage, steam_ids) in enumerate(
-                grouped_team_damages_dict.items(), start=1
-        ):
-            if counter + extra > 3:
-                break
-
-            prefix = f"^5{counter + extra:2}^7."
-            extra += max(0, len(steam_ids) - 1)
-
-            for steam_id in steam_ids:
-                player = self.player(steam_id)
-                if player is None:
-                    continue
-                self.msg(
-                    f"  {prefix} {player.name}^7 (^5{team_damage} team damage^7, "
-                    f"^5{len(self.team_damages[steam_id])} shots^7)"
-                )
-                prefix = "   "
 
     def handle_damage_event(self, target, attacker, dmg, dflags, means_of_death):
         timestamp = int(time.time() * 1000)
@@ -87,7 +48,7 @@ class team_shots(Plugin):
 
         if target is None:
             return
-        
+
         if attacker is None:
             return
 
@@ -95,6 +56,9 @@ class team_shots(Plugin):
             return
 
         if target.team != attacker.team:
+            return
+
+        if target.health <= 0:
             return
 
         if dmg < self.min_damage:
@@ -113,10 +77,10 @@ class team_shots(Plugin):
             self.play_sound("sound/feedback/hit_teammate.ogg", player=attacker)
 
             if (
-                    attacker.steam_id in self.team_damages
-                    and timestamp
-                    - max(self.team_damages[attacker.steam_id], key=itemgetter(1))[0]
-                    < 2000
+                attacker.steam_id in self.team_damages
+                and timestamp
+                - max(self.team_damages[attacker.steam_id], key=itemgetter(1))[0]
+                < 2000
             ):
                 attacker.center_print(self.warning_text)
 
