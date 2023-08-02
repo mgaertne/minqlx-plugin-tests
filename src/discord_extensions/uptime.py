b4d0e34c48bdd106cab2226d6a6ae55fab7d27c0
mgaertne <mgaertne@gmail.com>
2023-08-02 19:46:38 +0200

    added an uptime experimental plugin that shows server uptime information on the server as well as a discord extension

---
+++ src/discord_extensions/uptime.py
@@ -0,0 +1,48 @@
+import datetime
+import os
+import platform
+from typing import Union
+
+import distro
+import humanize
+import psutil
+# noinspection PyPackageRequirements
+from discord import Member, User, Embed, Color
+
+# noinspection PyPackageRequirements
+from discord import app_commands
+
+import minqlx
+from minqlx import Plugin
+
+
+@app_commands.context_menu(name="uptime")
+@app_commands.guild_only()
+async def uptime(interaction, _item: Union[Member, User]):
+    await _uptime(interaction)
+
+
+async def _uptime(interaction):
+    now = datetime.datetime.now()
+    lsb_info = distro.lsb_release_info()
+    os_boottime = datetime.datetime.fromtimestamp(psutil.boot_time())
+    os_uptime = humanize.precisedelta(now - os_boottime, minimum_unit="minutes", format="%d")
+
+    myself_process = psutil.Process(os.getpid())
+    qlserver_starttime = datetime.datetime.fromtimestamp(myself_process.create_time())
+    qlserver_uptime = humanize.precisedelta(now - qlserver_starttime, minimum_unit="minutes", format="%d")
+
+    minqlx_version = str(minqlx.__version__)[1:-1]
+
+    embed = Embed(color=Color.blurple())
+    title = Plugin.get_cvar("sv_hostname")
+    embed.title = title
+    embed.description = f"Operating system: {lsb_info['description']}, uptime: {os_uptime}\n" \
+                        f"Quake Live server running with minqlx {minqlx_version} (Python {platform.python_version()}) " \
+                        f"uptime: {qlserver_uptime}"
+    
+    await interaction.response.send_message(embed=embed, ephemeral=True)
+
+
+async def setup(bot):
+    bot.tree.add_command(uptime)
---
+++ src/experimental/uptime.py
@@ -0,0 +1,34 @@
+import datetime
+import os
+import platform
+
+import distro
+import psutil
+import humanize
+
+from minqlx import Plugin
+import minqlx
+
+
+# noinspection PyPep8Naming
+class uptime(Plugin):
+    def __init__(self):
+        super().__init__()
+        
+        self.add_command("uptime", self.cmd_uptime)
+        
+    def cmd_uptime(self, _player, _msg, _channel):
+        now = datetime.datetime.now()
+        lsb_info = distro.lsb_release_info()
+        os_boottime = datetime.datetime.fromtimestamp(psutil.boot_time())
+        os_uptime = humanize.precisedelta(now - os_boottime, minimum_unit="minutes", format="%d")
+
+        myself_process = psutil.Process(os.getpid())
+        qlserver_starttime = datetime.datetime.fromtimestamp(myself_process.create_time())
+        qlserver_uptime = humanize.precisedelta(now - qlserver_starttime, minimum_unit="minutes", format="%d")
+
+        minqlx_version = str(minqlx.__version__)[1:-1]        
+
+        self.msg(f"^6Operating system^7: ^5{lsb_info['description']}^7, ^6uptime^7: ^5{os_uptime}^7")
+        self.msg(f"^6Quake Live server^7 running with ^5minqlx {minqlx_version}^7 (Python {platform.python_version()}) "
+                 f"^6uptime: ^5{qlserver_uptime}^7")
