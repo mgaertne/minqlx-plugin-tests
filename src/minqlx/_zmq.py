36153e5d15e6b36100ef81616645793678a23772
mgaertne <mgaertne@gmail.com>
2023-11-09 12:20:06 +0100

    reworked the zmq python side to make use of the latest pyzmq interfaces and get rid of the bare exception catching

--- src/minqlx/_zmq.py
+++ src/minqlx/_zmq.py
@@ -19,9 +19,7 @@
 """Subscribes to the ZMQ stats protocol and calls the stats event dispatcher when
 we get stats from it. It polls the ZMQ socket approx. every 0.25 seconds."""
 
-import json
 
-# noinspection PyPackageRequirements
 import zmq
 import minqlx
 
@@ -29,113 +27,95 @@ import minqlx
 class StatsListener:
     def __init__(self):
         self.done = False
+        self._in_progress = False
+
         if not bool(int(minqlx.get_cvar("zmq_stats_enable"))):
             self.done = True
             return
 
-        stats = minqlx.get_cvar("zmq_stats_ip")
-        port = minqlx.get_cvar("zmq_stats_port")
-        if not port:
-            port = minqlx.get_cvar("net_port")
-        host = stats if stats else "127.0.0.1"
+        host = minqlx.get_cvar("zmq_stats_ip") or "127.0.0.1"
+        port = minqlx.get_cvar("zmq_stats_port") or minqlx.get_cvar("net_port")
         self.address = f"tcp://{host}:{port}"
         self.password = minqlx.get_cvar("zmq_stats_password")
 
-        # Initialize socket, connect, and subscribe.
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.SUB)
-        if self.password:
-            self.socket.plain_username = b"stats"
-            self.socket.plain_password = self.password.encode()
-        self.socket.zap_domain = b"stats"
-        self.socket.connect(self.address)
-        self.socket.setsockopt_string(zmq.SUBSCRIBE, "")
-
-        self.done = False
-        self._in_progress = False
-
     @minqlx.delay(0.25)
     def keep_receiving(self):
-        """Receives until 'self.done' is set to True. Works by scheduling this
-        to be called every 0.25 seconds. If we get an exception, we try
-        to reconnect and continue.
+        """Receives until 'self.done' is set to True."""
+        if self.done:
+            return
 
-        """
-        # noinspection PyBroadException
-        try:
-            if self.done:
-                return
+        with zmq.Context().instance().socket(zmq.SUB) as socket:
+            socket.setsockopt_string(zmq.PLAIN_USERNAME, "stats")
+            socket.setsockopt_string(zmq.PLAIN_PASSWORD, self.password)
+            socket.setsockopt_string(zmq.ZAP_DOMAIN, "stats")
+            socket.connect(self.address)
+            socket.subscribe("")
+
+            poller = zmq.Poller()
+            poller.register(socket, zmq.POLLIN)
+
             while True:  # Will throw an expcetion if no more data to get.
-                stats = json.loads(
-                    self.socket.recv(zmq.NOBLOCK).decode(errors="ignore")
-                )
-                minqlx.EVENT_DISPATCHERS["stats"].dispatch(stats)
+                pending_events = dict(poller.poll(timeout=250))
+                for receiver in pending_events:
+                    stats = receiver.recv.json()
+                    minqlx.EVENT_DISPATCHERS["stats"].dispatch(stats)
 
-                if stats["TYPE"] == "MATCH_STARTED":
-                    self._in_progress = True
-                    minqlx.EVENT_DISPATCHERS["game_start"].dispatch(stats["DATA"])
-                elif stats["TYPE"] == "ROUND_OVER":
-                    minqlx.EVENT_DISPATCHERS["round_end"].dispatch(stats["DATA"])
-                elif stats["TYPE"] == "MATCH_REPORT":
-                    # MATCH_REPORT event goes off with a map change and map_restart,
-                    # but we really only want it for when the game actually ends.
-                    # We use a variable instead of Game().state because by the
-                    # time we get the event, the game is probably gone.
-                    if self._in_progress:
-                        minqlx.EVENT_DISPATCHERS["game_end"].dispatch(stats["DATA"])
-                    self._in_progress = False
-                elif stats["TYPE"] == "PLAYER_DEATH":
-                    # Dead player.
-                    sid = int(stats["DATA"]["VICTIM"]["STEAM_ID"])
-                    player = (
-                        minqlx.Plugin.player(sid)
-                        if sid
-                        else minqlx.Plugin.player(stats["DATA"]["VICTIM"]["NAME"])
-                    )
+                    if stats["TYPE"] == "MATCH_STARTED":
+                        self._in_progress = True
+                        minqlx.EVENT_DISPATCHERS["game_start"].dispatch(stats["DATA"])
+                    elif stats["TYPE"] == "ROUND_OVER":
+                        minqlx.EVENT_DISPATCHERS["round_end"].dispatch(stats["DATA"])
+                    elif stats["TYPE"] == "MATCH_REPORT":
+                        # MATCH_REPORT event goes off with a map change and map_restart,
+                        # but we really only want it for when the game actually ends.
+                        # We use a variable instead of Game().state because by the
+                        # time we get the event, the game is probably gone.
+                        if self._in_progress:
+                            minqlx.EVENT_DISPATCHERS["game_end"].dispatch(stats["DATA"])
+                        self._in_progress = False
+                    elif stats["TYPE"] == "PLAYER_DEATH":
+                        # Dead player.
+                        sid = int(stats["DATA"]["VICTIM"]["STEAM_ID"])
+                        player = (
+                            minqlx.Plugin.player(sid)
+                            if sid
+                            else minqlx.Plugin.player(stats["DATA"]["VICTIM"]["NAME"])
+                        )
 
-                    # Killer player.
-                    if not stats["DATA"]["KILLER"]:
-                        player_killer = None
-                    else:
-                        sid_killer = int(stats["DATA"]["KILLER"]["STEAM_ID"])
-                        if sid_killer:
-                            player_killer = minqlx.Plugin.player(sid_killer)
-                        else:  # It's a bot. Forced to use name as an identifier.
-                            player_killer = minqlx.Plugin.player(
-                                stats["DATA"]["KILLER"]["NAME"]
-                            )
+                        # Killer player.
+                        if not stats["DATA"]["KILLER"]:
+                            player_killer = None
+                        else:
+                            sid_killer = int(stats["DATA"]["KILLER"]["STEAM_ID"])
+                            if sid_killer:
+                                player_killer = minqlx.Plugin.player(sid_killer)
+                            else:  # It's a bot. Forced to use name as an identifier.
+                                player_killer = minqlx.Plugin.player(
+                                    stats["DATA"]["KILLER"]["NAME"]
+                                )
 
-                    minqlx.EVENT_DISPATCHERS["death"].dispatch(
-                        player, player_killer, stats["DATA"]
-                    )
-                    if player_killer:
-                        minqlx.EVENT_DISPATCHERS["kill"].dispatch(
+                        minqlx.EVENT_DISPATCHERS["death"].dispatch(
                             player, player_killer, stats["DATA"]
                         )
-                elif stats["TYPE"] == "PLAYER_SWITCHTEAM":
-                    # No idea why they named it "KILLER" here, but whatever.
-                    player = minqlx.Plugin.player(
-                        int(stats["DATA"]["KILLER"]["STEAM_ID"])
-                    )
-                    if player is None:
-                        continue
-                    old_team = stats["DATA"]["KILLER"]["OLD_TEAM"].lower()
-                    new_team = stats["DATA"]["KILLER"]["TEAM"].lower()
-                    if old_team != new_team:
-                        res = minqlx.EVENT_DISPATCHERS["team_switch"].dispatch(
-                            player, old_team, new_team
+                        if player_killer:
+                            minqlx.EVENT_DISPATCHERS["kill"].dispatch(
+                                player, player_killer, stats["DATA"]
+                            )
+                    elif stats["TYPE"] == "PLAYER_SWITCHTEAM":
+                        # No idea why they named it "KILLER" here, but whatever.
+                        player = minqlx.Plugin.player(
+                            int(stats["DATA"]["KILLER"]["STEAM_ID"])
                         )
-                        if res is False:
-                            player.put(old_team)
-
-        except zmq.error.Again:
-            pass
-        except Exception:
-            minqlx.log_exception()
-            # Reconnect, just in case. GC will clean up for us.
-            self.__init__()  # type: ignore
-
-        self.keep_receiving()
+                        if player is None:
+                            continue
+                        old_team = stats["DATA"]["KILLER"]["OLD_TEAM"].lower()
+                        new_team = stats["DATA"]["KILLER"]["TEAM"].lower()
+                        if old_team != new_team:
+                            res = minqlx.EVENT_DISPATCHERS["team_switch"].dispatch(
+                                player, old_team, new_team
+                            )
+                            if res is False:
+                                player.put(old_team)
 
     def stop(self):
         self.done = True
--- stubs/minqlx/_zmq.pyi
+++ stubs/minqlx/_zmq.pyi
@@ -1,16 +1,7 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    # noinspection PyPackageRequirements
-    from zmq import Context, Socket
-
 class StatsListener:
     done: bool
     address: str
     password: str | None
-
-    context: Context
-    socket: Socket
     _in_progress: bool
 
     def __init__(self) -> None: ...
